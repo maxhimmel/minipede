34bedf0d1358e2919b7a0485e1d8ef13f420ec17
Max <himmelmax@gmail.com>
2022-10-08 08:40:34 -0700

    added health settings for player and blocks

--- Assets/Scripts/GameplaySettings.cs
+++ Assets/Scripts/GameplaySettings.cs
@@ -1,3 +1,4 @@
+using Minipede.Gameplay;
 using Minipede.Gameplay.Movement;
 using UnityEngine;
 using Zenject;
@@ -8,16 +9,28 @@ namespace Minipede.Installers
     public class GameplaySettings : ScriptableObjectInstaller
     {
 		[SerializeField] private Player _playerSettings;
+		[SerializeField] private Block _blockSettings;
 
 		public override void InstallBindings()
 		{
 			Container.BindInstance( _playerSettings );
+
+			Container.Bind<HealthController>()
+				.WithArguments( _blockSettings.Health )
+				.WhenInjectedInto<Gameplay.LevelPieces.Block>();
 		}
 
 		[System.Serializable]
         public struct Player
 		{
 			public CharacterMotor.Settings Movement;
+			public HealthController.Settings Health;
+		}
+
+		[System.Serializable]
+		public struct Block
+		{
+			public HealthController.Settings Health;
 		}
 	}
 }

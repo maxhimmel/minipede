c01c3bf8f7fb545ffba096f83ae30dc8268364e0
Max <himmelmax@gmail.com>
2022-10-11 16:12:03 -0700

    blocks can die and animate damage taken

--- Assets/Scripts/LevelPieces/Block.cs
+++ Assets/Scripts/LevelPieces/Block.cs
@@ -7,20 +7,38 @@ namespace Minipede.Gameplay.LevelPieces
 		IDamageable
 	{
 		private HealthController _health;
+		private SpriteRenderer _renderer;
 
 		[Inject]
-		public void Construct( HealthController health )
+		public void Construct( HealthController health,
+			SpriteRenderer renderer )
 		{
 			_health = health;
+			_renderer = renderer;
 		}
 
 		public int TakeDamage( Transform instigator, Transform causer, DamageDatum data )
 		{
 			int dmgDealt = _health.TakeDamage( data );
-
 			Debug.LogFormat( data.LogFormat(), name, dmgDealt, instigator?.name, causer?.name );
 
+			HandleDamageAnim();
+			if ( !_health.IsAlive )
+			{
+				HandleDeath();
+			}
+
 			return dmgDealt;
+		}
+
+		private void HandleDamageAnim()
+		{
+			_renderer.transform.localScale = Vector3.one * _health.Percentage;
+		}
+
+		private void HandleDeath()
+		{
+			Destroy( gameObject );
 		}
 	}
 }

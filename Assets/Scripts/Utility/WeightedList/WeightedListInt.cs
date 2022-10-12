a3a84be61e2c2b039944e0b5bdfb34455bd7fb3f
Max <himmelmax@gmail.com>
2022-10-12 13:12:03 -0700

    added integer weighted list

---
+++ Assets/Scripts/Utility/WeightedList/WeightedListInt.cs
@@ -0,0 +1,8 @@
+namespace Minipede.Utility
+{
+    [System.Serializable]
+    public class WeightedListInt : WeightedList<WeightedNodeInt, int> { }
+
+    [System.Serializable]
+    public class WeightedNodeInt : WeightedNode<int> { }
+}

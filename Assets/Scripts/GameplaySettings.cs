118625fcad63008fd644dac4f9f1f4bfc3aa084e
Max <himmelmax@gmail.com>
2022-10-12 13:15:42 -0700

    added settings for level generation

--- Assets/Scripts/GameplaySettings.cs
+++ Assets/Scripts/GameplaySettings.cs
@@ -1,7 +1,12 @@
 using Minipede.Gameplay;
+using Minipede.Gameplay.LevelPieces;
 using Minipede.Gameplay.Movement;
+using Minipede.Utility;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using Zenject;
+
+using BlockPiece = Minipede.Gameplay.LevelPieces.Block;
 
 namespace Minipede.Installers
 {
@@ -10,6 +15,7 @@ namespace Minipede.Installers
     {
 		[SerializeField] private Player _playerSettings;
 		[SerializeField] private Block _blockSettings;
+		[SerializeField] private Level _levelSettings;
 
 		public override void InstallBindings()
 		{
@@ -17,7 +23,12 @@ namespace Minipede.Installers
 
 			Container.Bind<HealthController>()
 				.WithArguments( _blockSettings.Health )
-				.WhenInjectedInto<Gameplay.LevelPieces.Block>();
+				.WhenInjectedInto<BlockPiece>();
+
+			Container.BindInstance( _levelSettings );
+
+			Container.BindFactory<BlockPiece, BlockPiece.Factory>()
+				.FromComponentInNewPrefab( _levelSettings.BlockPrefab );
 		}
 
 		[System.Serializable]
@@ -31,6 +42,16 @@ namespace Minipede.Installers
 		public struct Block
 		{
 			public HealthController.Settings Health;
+		}
+
+		[System.Serializable]
+		public struct Level
+		{
+			public BlockPiece BlockPrefab;
+			public LevelGraph.Settings Graph;
+
+			[TabGroup("Blocks Per Row")]
+			public WeightedListInt RowGeneration;
 		}
 	}
 }

b7cf2e826a748a4d385bf69ada80a5645258d40f
Max <himmelmax@gmail.com>
2022-10-13 21:52:25 -0700

    added vector helpers

--- Assets/Scripts/Utility/Extensions/VectorExtensions.cs
+++ Assets/Scripts/Utility/Extensions/VectorExtensions.cs
@@ -13,5 +13,37 @@ namespace Minipede.Utility
         {
             return self.y;
         }
+
+        public static bool Approximately( this Vector3 self, Vector3 other )
+		{
+            if ( !Mathf.Approximately( self.x, other.x ) )
+			{
+                return false;
+            }
+            if ( !Mathf.Approximately( self.y, other.y ) )
+            {
+                return false;
+            }
+            if ( !Mathf.Approximately( self.z, other.z ) )
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool Approximately( this Vector2 self, Vector2 other )
+        {
+            if ( !Mathf.Approximately( self.x, other.x ) )
+            {
+                return false;
+            }
+            if ( !Mathf.Approximately( self.y, other.y ) )
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }

722da3b46c15a271d08fa7101e4a44111f3229d9
Max <himmelmax@gmail.com>
2022-10-16 21:36:19 -0700

    graph settings is now public
    also added world space helper

--- Assets/Scripts/Utility/LevelGraphNavigator.cs
+++ Assets/Scripts/Utility/LevelGraphNavigator.cs
@@ -6,8 +6,7 @@ namespace Minipede.Utility
     public class LevelGraphNavigator
 	{
 		public LevelGraph Graph { get; }
-
-		private readonly LevelGraph.Settings _settings;
+		public LevelGraph.Settings Settings { get; }
 
 		private Vector2 _initialOrigin;
 		private Vector2 _localOrigin;
@@ -15,7 +14,7 @@ namespace Minipede.Utility
 		public LevelGraphNavigator( LevelGraph levelGraph )
 		{
 			Graph = levelGraph;
-			_settings = levelGraph.GraphSettings;
+			Settings = levelGraph.GraphSettings;
 
 			TryUpdateLocalOriginCache( forceUpdate: true );
 		}
@@ -27,11 +26,11 @@ namespace Minipede.Utility
 			Vector2Int cellCoord = GetCellCoordinates( worldPosition );
 
 			cellData = null;
-			if ( cellCoord.Row() < 0 || cellCoord.Row() >= _settings.Dimensions.Row() )
+			if ( cellCoord.Row() < 0 || cellCoord.Row() >= Settings.Dimensions.Row() )
 			{
 				return false;
 			}
-			if ( cellCoord.Col() < 0 || cellCoord.Col() >= _settings.Dimensions.Col() )
+			if ( cellCoord.Col() < 0 || cellCoord.Col() >= Settings.Dimensions.Col() )
 			{
 				return false;
 			}
@@ -44,8 +43,8 @@ namespace Minipede.Utility
 		{
 			TryUpdateLocalOriginCache();
 
-			worldPosition.x /= _settings.Size.x;
-			worldPosition.y /= _settings.Size.y;
+			worldPosition.x /= Settings.Size.x;
+			worldPosition.y /= Settings.Size.y;
 
 			Vector2 localPos = worldPosition - _localOrigin;
 
@@ -66,18 +65,23 @@ namespace Minipede.Utility
 			_initialOrigin = Graph.transform.position;
 			_localOrigin = Graph.transform.position;
 
-			_localOrigin += _settings.Size * 0.5f;
-			_localOrigin += _settings.Offset;
+			_localOrigin += Settings.Size * 0.5f;
+			_localOrigin += Settings.Offset;
 
-			_localOrigin.x /= _settings.Size.x;
-			_localOrigin.y /= _settings.Size.y;
+			_localOrigin.x /= Settings.Size.x;
+			_localOrigin.y /= Settings.Size.y;
 
 			return true;
 		}
 
 		public LevelCell GetClosestCell( Vector2 worldPosition )
 		{
-			var cellCoords = GetCellCoordinates( worldPosition );
+			return GetClosestCell( worldPosition, out _ );
+		}
+
+		public LevelCell GetClosestCell( Vector2 worldPosition, out Vector2Int cellCoords )
+		{
+			cellCoords = GetCellCoordinates( worldPosition );
 			cellCoords = GetClosestValidCellCoordinate( cellCoords );
 
 			return Graph.GetCellData( cellCoords.Row(), cellCoords.Col() );
@@ -85,8 +89,8 @@ namespace Minipede.Utility
 
 		public Vector2Int GetClosestValidCellCoordinate( Vector2Int cellCoord )
 		{
-			bool isRowValid = cellCoord.Row() >= 0 && cellCoord.Row() < _settings.Dimensions.Row();
-			bool isColumnValid = cellCoord.Col() >= 0 && cellCoord.Col() < _settings.Dimensions.Col();
+			bool isRowValid = cellCoord.Row() >= 0 && cellCoord.Row() < Settings.Dimensions.Row();
+			bool isColumnValid = cellCoord.Col() >= 0 && cellCoord.Col() < Settings.Dimensions.Col();
 			if ( isRowValid && isColumnValid )
 			{
 				return cellCoord;
@@ -106,6 +110,11 @@ namespace Minipede.Utility
 			}
 
 			return cellCoord;
+		}
+
+		public Vector2 GetCellWorldPosition( Vector2Int cellCoord )
+		{
+			return Graph.CellCoordToWorldPosition( cellCoord.Row(), cellCoord.Col() );
 		}
 	}
 }

f2a33c95f8957116e95cbcfb66b1ba6358f7171e
Max <himmelmax@gmail.com>
2022-10-17 10:58:14 -0700

    cleaner way of handling beetle movement routine

--- Assets/Scripts/Enemies/BeetleController.cs
+++ Assets/Scripts/Enemies/BeetleController.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Minipede.Gameplay.LevelPieces;
 using Minipede.Gameplay.Movement;
 using Minipede.Utility;
@@ -21,8 +19,6 @@ namespace Minipede.Gameplay.Enemies
 		private LevelGraph _levelGraph;
 		private LevelForeman _levelForeman;
 
-		private Queue<Vector2Int> _moveQueue = new Queue<Vector2Int>();
-
 		[Inject]
 		public void Construct( Settings settings,
 			IRemoteMotor motor,
@@ -39,108 +35,55 @@ namespace Minipede.Gameplay.Enemies
 			_damageController = damageController;
 			_levelGraph = levelGraph;
 			_levelForeman = levelForeman;
-
-			FillMoveQueue();
 		}
 
-		//private IEnumerator Start()
-		//{
-		//	while ( !_gameController.IsReady )
-		//	{
-		//		yield return null;
-		//	}
-
-		//	yield return NavigationRoutine();
-
-		//	Destroy( gameObject );
-		//}
-
-		//private IEnumerator NavigationRoutine()
-		//{
-		//	// Move out of spawn area ...
-		//	Vector2Int cellCoord = _levelGraph.WorldPosToClampedCellCoord( _body.position );
-		//	yield return WaitForMovement( cellCoord );
-
-		//	// Move down to bottom of player area ...
-		//	cellCoord.x = 0;
-		//	yield return WaitForMovement( cellCoord );
-
-		//	// Move towards center ...
-		//	cellCoord.y = _settings.ColumnMovementRange.Random( true );
-		//	yield return WaitForMovement( cellCoord );
-
-		//	// Move up towards exit row ...
-		//	cellCoord.x = _settings.ExitRow;
-		//	yield return WaitForMovement( cellCoord );
-
-		//	// Exit off screen ...
-		//	cellCoord.y = Vector2.Dot( transform.up, Vector2.right ) > 0
-		//		? _levelGraph.Data.Dimensions.Col()
-		//		: -1;
-		//	yield return WaitForMovement( cellCoord );
-		//}
-
-		//private IEnumerator WaitForMovement( Vector2Int cellCoordDest )
-		//{
-		//	Vector2 nextPos = _levelGraph.CellCoordToWorldPos( cellCoordDest );
-		//	_motor.StartMoving( GetMoveDirection( nextPos ) );
-
-		//	while ( _motor.IsMoving )
-		//	{
-		//		TryCreateFlowers();
-		//		yield return new WaitForFixedUpdate();
-		//	}
-		//}
-
-		private void FillMoveQueue()
+		private IEnumerator Start()
 		{
-			_moveQueue.Clear();
+			while ( !_gameController.IsReady )
+			{
+				yield return new WaitForFixedUpdate();
+			}
 
+			yield return NavigationRoutine();
+
+			Destroy( gameObject );
+		}
+
+		private IEnumerator NavigationRoutine()
+		{
 			// Move out of spawn area ...
 			Vector2Int cellCoord = _levelGraph.WorldPosToClampedCellCoord( _body.position );
-			_moveQueue.Enqueue( cellCoord );
+			yield return WaitForMovement( cellCoord );
 
 			// Move down to bottom of player area ...
 			cellCoord.x = 0;
-			_moveQueue.Enqueue( cellCoord );
+			yield return WaitForMovement( cellCoord );
 
 			// Move towards center ...
 			cellCoord.y = _settings.ColumnMovementRange.Random( true );
-			_moveQueue.Enqueue( cellCoord );
+			yield return WaitForMovement( cellCoord );
 
 			// Move up towards exit row ...
 			cellCoord.x = _settings.ExitRow;
-			_moveQueue.Enqueue( cellCoord );
+			yield return WaitForMovement( cellCoord );
 
 			// Exit off screen ...
 			cellCoord.y = Vector2.Dot( transform.up, Vector2.right ) > 0
 				? _levelGraph.Data.Dimensions.Col()
 				: -1;
-			_moveQueue.Enqueue( cellCoord );
+			yield return WaitForMovement( cellCoord );
 		}
 
-		private void FixedUpdate()
+		private IEnumerator WaitForMovement( Vector2Int cellCoordDest )
 		{
-			if ( !_gameController.IsReady )
-			{
-				return;
-			}
+			Vector2 nextPos = _levelGraph.CellCoordToWorldPos( cellCoordDest );
+			_motor.StartMoving( GetMoveDirection( nextPos ) );
 
-			if ( _moveQueue.Count <= 0 && !_motor.IsMoving )
+			while ( _motor.IsMoving )
 			{
-				Destroy( gameObject );
-				return;
+				TryCreateFlowers();
+				yield return new WaitForFixedUpdate();
 			}
-
-			if ( !_motor.IsMoving )
-			{
-				Vector2Int nextCoord = _moveQueue.Dequeue();
-				Vector2 nextPos = _levelGraph.CellCoordToWorldPos( nextCoord );
-				_motor.StartMoving( GetMoveDirection( nextPos ) );
-			}
-
-			_motor.FixedTick();
-			TryCreateFlowers();
 		}
 
 		private void TryCreateFlowers()
@@ -156,6 +99,11 @@ namespace Minipede.Gameplay.Enemies
 		private Vector2 GetMoveDirection( Vector2 position )
 		{
 			return position - _body.position;
+		}
+
+		private void FixedUpdate()
+		{
+			_motor.FixedTick();
 		}
 
 		public int TakeDamage( Transform instigator, Transform causer, DamageDatum data )

90c2480b6b592c1d5fa694cfa42c8381d65f85d1
Max <himmelmax@gmail.com>
2022-10-23 08:26:42 -0700

    minipede is working!!!!!!!
    will not perform initial movement without a spawner telling it what to do or supplying it follow segments

--- Assets/Scripts/Enemies/MinipedeController.cs
+++ Assets/Scripts/Enemies/MinipedeController.cs
@@ -14,134 +14,40 @@ namespace Minipede.Gameplay.Enemies
 	{
 		private Settings _settings;
 		private GraphMotor _motor;
-		private MinipedeSegmentController.Factory _followerFactory;
 		private Factory _minipedeFactory;
 
-		private List<MinipedeSegmentController> _followers;
+		private List<SegmentController> _segments;
 		private Vector2Int _rowDir;
 		private Vector2Int _columnDir;
 
 		[Inject]
 		public void Construct( Settings settings,
 			GraphMotor motor,
-			MinipedeSegmentController.Factory followerFactory,
 			Factory minipedeFactory )
 		{
 			_settings = settings;
 			_motor = motor;
-			_followerFactory = followerFactory;
 			_minipedeFactory = minipedeFactory;
 
 			_rowDir = Vector2Int.down;
 			_columnDir = new Vector2Int( RandomExtensions.Sign(), 0 );
 		}
 
-		public bool CanCreateFollowers = true;
-
 		protected override void OnReady()
 		{
 			base.OnReady();
 
-			if ( CanCreateFollowers )
-			{
-				CreateFollowers( _settings.SegmentRange.Random( true ) );
-			}
+			//StartRowTransition();
+		}
 
-			StartRowTransition()
+		public void StartRowTransition()
+		{
+			UpdateRowTransition()
 				.Cancellable( _onDestroyCancelToken )
 				.Forget();
 		}
 
-		private void CreateFollowers( int segmentCount )
-		{
-			Rigidbody2D followTarget = _body;
-
-			_followers = new List<MinipedeSegmentController>( segmentCount );
-			for ( int idx = 0; idx < segmentCount; ++idx )
-			{
-				MinipedeSegmentController newFollower = CreateFollower( followTarget, idx);
-				followTarget = newFollower.Body;
-			}
-
-			SetFollowers( _followers );
-		}
-
-		private MinipedeSegmentController CreateFollower( Rigidbody2D leader, int index )
-		{
-			Vector2 offsetDir = transform.right;
-			Vector2 spawnOffset = offsetDir * _levelGraph.Data.Size.x;// * (index + 1);
-			Vector2 spawnPos = leader.position + spawnOffset;
-			Quaternion spawnRot = Quaternion.LookRotation( Vector3.forward, -offsetDir );
-
-			MinipedeSegmentController newFollower = _followerFactory.Create( spawnPos, spawnRot, null );
-			newFollower.StartFollowing( leader );
-
-			_followers.Add( newFollower );
-			return newFollower;
-		}
-
-		public void SetFollowers( List<MinipedeSegmentController> followers )
-		{
-			_followers = followers;
-			for ( int idx = 0; idx < followers.Count; ++idx )
-			{
-				IFollower follower = followers[idx];
-				if ( follower is IDamageController dmgController )
-				{
-					dmgController.Died += OnSegmentDied;
-				}
-			}
-		}
-
-		private void OnSegmentDied( Rigidbody2D victimBody, HealthController health )
-		{
-			MinipedeSegmentController victim = victimBody.GetComponent<MinipedeSegmentController>();
-			if ( victim == null )
-			{
-				throw new System.NullReferenceException(
-					$"Segment '{victimBody.name}' must have a component implementing '{nameof( IFollower )}'."
-				);
-			}
-
-			int victimIndex = _followers.FindIndex( otherFollower => otherFollower == victim );
-			int nextFollowerIndex = victimIndex + 1;
-
-			if ( nextFollowerIndex < _followers.Count )
-			{
-				MinipedeSegmentController nextFollower = _followers[nextFollowerIndex];
-
-				MinipedeController leader = _minipedeFactory.Create(
-					nextFollower.Body.transform.position,
-					nextFollower.Body.transform.rotation,
-					null
-				);
-				leader.CanCreateFollowers = false;
-
-				Destroy( nextFollower.Body.gameObject );
-
-				++nextFollowerIndex;
-				if ( nextFollowerIndex < _followers.Count )
-				{
-					var newFollowers = _followers.GetRange( nextFollowerIndex, _followers.Count - nextFollowerIndex );
-					leader.SetFollowers( newFollowers );
-
-					var firstFollower = newFollowers[0];
-					firstFollower.StartFollowing( leader._body );
-				}
-			}
-
-			var splitFollowers = _followers.GetRange( victimIndex, _followers.Count - victimIndex );
-			foreach ( var otherFollower in splitFollowers )
-			{
-				if ( otherFollower is IDamageController dmgController )
-				{
-					dmgController.Died -= OnSegmentDied;
-				}
-			}
-			_followers.RemoveRange( victimIndex, _followers.Count - victimIndex );
-		}
-
-		private async UniTask StartRowTransition()
+		private async UniTask UpdateRowTransition()
 		{
 			Vector2Int nextRowCoord = _levelGraph.WorldPosToCellCoord( _body.position );
 			if ( !_levelGraph.IsWithinBounds( nextRowCoord + _rowDir.ToRowCol() ) )
@@ -162,9 +68,7 @@ namespace Minipede.Gameplay.Enemies
 			if ( _levelForeman.TryQueryFilledBlock( _body.position, out _ ) ||
 				WillCollideWithNextColumn( nextColCoord ) )
 			{
-				StartRowTransition()
-					.Cancellable( _onDestroyCancelToken )
-					.Forget();
+				StartRowTransition();
 				return;
 			}
 
@@ -179,7 +83,7 @@ namespace Minipede.Gameplay.Enemies
 				_motor.StopMoving();
 				_motor.Arrived -= OnHorizontalArrival;
 
-				StartRowTransition().Cancellable( _onDestroyCancelToken );
+				StartRowTransition();
 			}
 		}
 
@@ -211,20 +115,172 @@ namespace Minipede.Gameplay.Enemies
 			transform.rotation = Quaternion.RotateTowards( transform.rotation, targetRotation, rotationDelta );
 		}
 
+		public bool Pause = true;
+
+		private void Update()
+		{
+			if ( _segments == null || _segments.Count <= 0 )
+			{
+				return;
+			}
+
+			if ( Input.GetKeyDown( KeyCode.Alpha1 ) )
+			{
+				var tailSegment = _segments[_segments.Count - 1];
+				tailSegment.TakeDamage( transform, transform, new DamageDatum( 1 ) );
+
+				if ( Pause )
+				{
+					Debug.Break();
+				}
+			}
+			else if ( Input.GetKeyDown( KeyCode.Alpha2 ) )
+			{
+				var head = this;
+				head.TakeDamage( transform, transform, new DamageDatum( 1 ) );
+
+				if ( Pause )
+				{
+					Debug.Break();
+				}
+			}
+			else if ( Input.GetKeyDown( KeyCode.Alpha3 ) )
+			{
+				int randBodyIndex = Mathf.Max( 0, _segments.Count - 3 );//Random.Range( 0, _segments.Count - 1 );
+				Debug.Log( randBodyIndex );
+
+				var body = _segments[randBodyIndex];
+				body.TakeDamage( transform, transform, new DamageDatum( 1 ) );
+
+				if ( Pause )
+				{
+					Debug.Break();
+				}
+			}
+		}
+
+		private void OnGUI()
+		{
+			if ( _segments == null )
+			{
+				return;
+			}
+
+			for ( int idx = 0; idx < _segments.Count; ++idx )
+			{
+				if ( GUILayout.Button( $"{idx}" ) )
+				{
+					var body = _segments[idx];
+					body.TakeDamage( transform, transform, new DamageDatum( 1 ) );
+
+					if ( Pause )
+					{
+						Debug.Break();
+					}
+				}
+			}
+		}
+
 		protected override void OnDied( Rigidbody2D victimBody, HealthController health )
 		{
 			base.OnDied( victimBody, health );
 
-			//if ( _levelForeman != null && _levelGraph != null )
-			{
-				Vector2Int cellCoord = _levelGraph.WorldPosToCellCoord( victimBody.position );
-				cellCoord += _columnDir.ToRowCol();
-				Vector2 nextPos = _levelGraph.CellCoordToWorldPos( cellCoord );
+			TryCreateBlock( victimBody.position );
 
-				if ( _levelForeman.TryQueryEmptyBlock( nextPos, out var instructions ) )
+			if ( _segments != null && _segments.Count > 0 )
+			{
+				MinipedeController newHead = ReplaceSegmentWithHead( 0, victimBody.position );
+				if ( _segments.Count > 0 )
 				{
-					instructions.Create( Block.Type.Regular );
+					newHead.SetSegments( _segments );
 				}
+			}
+		}
+
+		private bool TryCreateBlock( Vector2 position )
+		{
+			Vector2Int cellCoord = _levelGraph.WorldPosToCellCoord( position );
+			cellCoord += _columnDir.ToRowCol();
+			Vector2 nextPos = _levelGraph.CellCoordToWorldPos( cellCoord );
+
+			_levelForeman.ClearQuery();
+			if ( _levelForeman.TryQueryEmptyBlock( nextPos, out var instructions ) )
+			{
+				instructions.Create( Block.Type.Regular );
+				return true;
+			}
+
+			return false;
+		}
+
+		private void OnSegmentDied( Rigidbody2D victimBody, HealthController health )
+		{
+			int victimIndex = RemoveDeadSegment( victimBody );
+			if ( victimIndex < _segments.Count )
+			{
+				MinipedeController newHead = ReplaceSegmentWithHead( victimIndex, victimBody.position );
+				if ( _segments.Count > 0 )
+				{
+					List<SegmentController> bottomHalf = _segments.GetRange( victimIndex, _segments.Count - victimIndex );
+					newHead.SetSegments( bottomHalf );
+				}
+
+				RemoveSegmentRange( _segments.Count - 1, victimIndex );
+			}
+		}
+
+		private int RemoveDeadSegment( Rigidbody2D victimBody )
+		{
+			SegmentController victimSegment = victimBody.GetComponent<SegmentController>();
+			Debug.Assert( victimSegment != null, new MissingComponentException( nameof( SegmentController ) ), this );
+
+			int victimIndex = _segments.FindIndex( other => other == victimSegment );
+			_segments.RemoveAt( victimIndex );
+
+			return victimIndex;
+		}
+
+		private MinipedeController ReplaceSegmentWithHead( int segmentIndex, Vector2 newHeadPosition )
+		{
+			SegmentController segment = _segments[segmentIndex];
+			Destroy( segment.gameObject );
+			_segments.RemoveAt( segmentIndex );
+
+			MinipedeController newHead = _minipedeFactory.Create( segment.transform.ToData() );
+			newHead._rowDir = _rowDir;
+			newHead._columnDir = _columnDir;
+
+			Vector2Int destCoord = _levelGraph.WorldPosToCellCoord( newHeadPosition );
+			newHead._motor.SetDestination( destCoord )
+				.ContinueWith( newHead.StartRowTransition )
+				.Cancellable( newHead._onDestroyCancelToken )
+				.Forget();
+
+			return newHead;
+		}
+
+		public void SetSegments( List<SegmentController> segments )
+		{
+			_segments = segments;
+			Rigidbody2D leader = _body;
+
+			foreach ( var segment in segments )
+			{
+				segment.Died += OnSegmentDied;
+
+				segment.StartFollowing( leader );
+				leader = segment.Body;
+			}
+		}
+
+		private void RemoveSegmentRange( int startIndex, int endIndex )
+		{
+			for ( int idx = startIndex; idx >= endIndex; --idx )
+			{
+				var segment = _segments[idx];
+				segment.Died -= OnSegmentDied;
+
+				_segments.RemoveAt( idx );
 			}
 		}
 
@@ -234,7 +290,7 @@ namespace Minipede.Gameplay.Enemies
 			[MinMaxSlider( 0, 10, ShowFields = true )]
 			public Vector2Int SegmentRange;
 
-			public MinipedeSegmentController SegmentPrefab;
+			public SegmentController SegmentPrefab;
 		}
 
 		public class Factory : UnityFactory<MinipedeController> { }

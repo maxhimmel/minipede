1b0b124c8b7dae8caba5d90bcfcda4cfe044e1da
Max <himmelmax@gmail.com>
2022-10-23 16:42:57 -0700

    using new spawn point system for the player

--- Assets/Scripts/LevelPieces/PlayerSpawner.cs
+++ Assets/Scripts/LevelPieces/PlayerSpawner.cs
@@ -8,7 +8,7 @@ namespace Minipede.Gameplay.LevelPieces
     public class PlayerSpawner
 	{
 		private readonly PlayerController.Factory _factory;
-		private readonly Transform _spawnPoint;
+		private readonly ISpawnPoint _spawnPoint;
 
 		public PlayerSpawner( 
 			DiContainer container,
@@ -16,14 +16,14 @@ namespace Minipede.Gameplay.LevelPieces
 			GameplaySettings.Player settings )
 		{
 			_factory = factory;
-			_spawnPoint = container.ResolveId<Transform>( settings.SpawnPointId );
+			_spawnPoint = container.ResolveId<ISpawnPoint>( settings.SpawnPointId );
 		}
 
 		public PlayerController Spawn()
 		{
 			var newPlayer = _factory.Create();
-			newPlayer.transform.SetParent( null );
-			newPlayer.transform.position = _spawnPoint.position;
+			newPlayer.transform.SetParent( _spawnPoint.Container );
+			newPlayer.transform.SetPositionAndRotation( _spawnPoint.Position, _spawnPoint.Rotation );
 
 			return newPlayer;
 		}

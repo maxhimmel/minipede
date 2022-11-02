a9c388671c102de31800b08a350cdea04897e084
Max <himmelmax@gmail.com>
2022-11-01 21:08:19 -0700

    WIP: attempting to spawn random enemies at random intervals
    (it's buggy)

--- Assets/Scripts/Enemies/Waves/MinipedeWave.cs
+++ Assets/Scripts/Enemies/Waves/MinipedeWave.cs
@@ -1,5 +1,10 @@
 using System.Linq;
+using Cysharp.Threading.Tasks;
 using Minipede.Utility;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Zenject;
 
 namespace Minipede.Gameplay.Enemies.Spawning
 {
@@ -9,9 +14,20 @@ namespace Minipede.Gameplay.Enemies.Spawning
 
 		private int _completionCount;
 
-		public MinipedeWave( Settings settings )
+		public MinipedeWave( Settings settings,
+			IEnemyWave.Settings globalSettings, 
+			EnemySpawnBuilder enemyBuilder,
+			EnemyPlacementResolver placementResolver, 
+			SignalBus signalBus ) 
+			: base( globalSettings, enemyBuilder, placementResolver, signalBus )
 		{
 			_settings = settings;
+
+			settings.Enemies.Init();
+			foreach ( var factory in settings.Enemies.Items )
+			{
+				factory.EnemyBuilder = enemyBuilder;
+			}
 		}
 
 		protected override void HandleSpawning()
@@ -20,15 +36,44 @@ namespace Minipede.Gameplay.Enemies.Spawning
 			spawnPlacements.FisherYatesShuffle();
 
 			int nextSpawnIndex = 0;
-			_spawnerBus.Create<MinipedeController>( spawnPlacements[nextSpawnIndex] );
+
+			_enemyBuilder.Build<MinipedeController>()
+				.WithPlacement( spawnPlacements[nextSpawnIndex] )
+				.WithSpawnBehavior()
+				.Create();
 
 			int headSpawnCount = _completionCount;
 			for ( int idx = 0; idx < headSpawnCount; ++idx )
 			{
 				var spawnOrientation = spawnPlacements[++nextSpawnIndex];
 
-				MinipedeController minipedeHead = _factoryBus.Create<MinipedeController>( spawnOrientation );
-				minipedeHead.OnSpawned();
+				_enemyBuilder.Build<MinipedeController>()
+					.WithPlacement( spawnOrientation )
+					.Create()
+					.OnSpawned();
+			}
+
+			UpdateRandomSpawning()
+				.Cancellable( _playerDiedCancelToken )
+				.Forget();
+		}
+
+		private async UniTask UpdateRandomSpawning()
+		{
+			float nextSpawnCountdown = _settings.SpawnRateRange.Random();
+
+			while ( IsRunning )
+			{
+				if ( nextSpawnCountdown <= 0 )
+				{
+					nextSpawnCountdown = _settings.SpawnRateRange.Random();
+
+					var spawner = _settings.Enemies.GetRandomItem();
+					spawner.Create();
+				}
+
+				nextSpawnCountdown -= Time.deltaTime;
+				await UniTask.Yield();
 			}
 		}
 
@@ -69,6 +114,12 @@ namespace Minipede.Gameplay.Enemies.Spawning
 		public struct Settings
 		{
 			public int Repeats;
+
+			[Space, MinMaxSlider( 1, 10 )]
+			public Vector2 SpawnRateRange;
+
+			[BoxGroup]
+			public Serialization.WeightedListEnemy Enemies;
 		}
 	}
 }

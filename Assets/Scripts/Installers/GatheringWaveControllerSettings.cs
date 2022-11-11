96339696d49ddf60cb6ae0091bca197bebdc2a56
Max <himmelmax@gmail.com>
2022-11-10 21:13:01 -0800

    added gathering phase!

---
+++ Assets/Scripts/Installers/GatheringWaveControllerSettings.cs
@@ -0,0 +1,20 @@
+﻿using Minipede.Gameplay.Gathering;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Installers
+{
+	[CreateAssetMenu( menuName = "Gathering Wave Settings" )]
+	public class GatheringWaveControllerSettings : ScriptableObjectInstaller
+	{
+		[SerializeField] private GatheringWaveController.Settings _settings;
+
+		public override void InstallBindings()
+		{
+			Container.Bind<GatheringWaveController>()
+				.AsSingle()
+				.WithArguments( _settings )
+				.NonLazy();
+		}
+	}
+}

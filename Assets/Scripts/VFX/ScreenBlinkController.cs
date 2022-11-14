ecbeafd63e34d0d2f76856177ea6af236dce2ba2
Max <himmelmax@gmail.com>
2022-11-13 17:45:05 -0800

    SEIZURE WARNING - created screen blinking VFX!
    it's so juicy!

---
+++ Assets/Scripts/VFX/ScreenBlinkController.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Gameplay.Vfx
+{
+    public class ScreenBlinkController : ITickable
+	{
+		private readonly UnityEngine.Camera _camera;
+		private readonly SignalBus _signalBus;
+		private readonly List<BlinkData> _blinks;
+
+		private Color _baseColor;
+
+		public ScreenBlinkController( UnityEngine.Camera camera )
+		{
+			_camera = camera;
+			_blinks = new List<BlinkData>();
+
+			SetBaseColor( camera.backgroundColor );
+		}
+
+		public void SetBaseColor( Color color )
+		{
+			_baseColor = color;
+		}
+
+		public void Blink( Settings settings )
+		{
+			_blinks.Add( new BlinkData( settings.Color, settings.Duration ) );
+		}
+
+		public void Tick()
+		{
+			Color sum = _baseColor;
+
+			for ( int idx = _blinks.Count - 1; idx >= 0; --idx )
+			{
+				var blink = _blinks[idx];
+				sum += blink.Evaluate( Color.clear, out bool completed );
+
+				if ( completed )
+				{
+					_blinks.RemoveAt( idx );
+				}
+			}
+
+			_camera.backgroundColor = sum;
+		}
+
+		private class BlinkData
+		{
+			public Color Color { get; }
+			public float Duration { get; }
+
+			private float _timer;
+
+			public BlinkData( Color color, float duration )
+			{
+				Color = color;
+				Duration = duration;
+			}
+
+			public Color Evaluate( Color blendTo, out bool isComplete )
+			{
+				_timer += Time.deltaTime / Duration;
+				_timer = Mathf.Min( _timer, 1 );
+
+				isComplete = _timer >= 1;
+				return Color.LerpUnclamped( Color, blendTo, _timer );
+			}
+		}
+
+		[System.Serializable]
+		public struct Settings
+		{
+			public Color Color;
+			public float Duration;
+		}
+	}
+}

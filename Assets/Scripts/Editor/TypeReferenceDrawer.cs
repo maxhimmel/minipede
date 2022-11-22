c3fb536305ceb3f8fd1b464cb6c2c158a5a236f9
Max <himmelmax@gmail.com>
2022-11-22 08:03:59 -0800

    serialize type reference names

---
+++ Assets/Scripts/Editor/TypeReferenceDrawer.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using Minipede.Utility;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Minipede.Editor
+{
+	public class TypeReferenceDrawer : OdinAttributeDrawer<TypeReferenceAttribute, TypeReference>
+	{
+		private TypeSelector _typeSelector;
+		private GUIStyle _buttonStyle;
+
+		protected override void Initialize()
+		{
+			base.Initialize();
+
+			_typeSelector = new TypeSelector( this.Attribute.GetTypes(), false );
+			_typeSelector.FlattenTree = true;
+			_typeSelector.EnableSingleClickToSelect();
+			_typeSelector.SelectionConfirmed += ( selection ) =>
+			{
+				this.ValueEntry.SmartValue.Fullname = selection.FirstOrDefault().FullName;
+			};
+
+			_buttonStyle = SirenixGUIStyles.DropDownMiniButton;
+			_buttonStyle.alignment = TextAnchor.MiddleLeft;
+		}
+
+		protected override void DrawPropertyLayout( GUIContent label )
+		{
+			SirenixEditorGUI.BeginHorizontalPropertyLayout( label );
+			{
+				string typeName = this.ValueEntry.SmartValue.IsValid ? this.ValueEntry.SmartValue.RefType.Name : "null";
+				if ( GUILayout.Button( typeName, _buttonStyle ) )
+				{
+					_typeSelector.ShowInPopup();
+				}
+			}
+			SirenixEditorGUI.EndHorizontalPropertyLayout();
+
+			EditorGUILayout.Space( EditorGUIUtility.standardVerticalSpacing );
+		}
+	}
+}
---
+++ Assets/Scripts/Utility/TypeReference.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Minipede.Utility
+{
+	[Serializable]
+	public class TypeReference
+	{
+		public bool IsValid => !string.IsNullOrEmpty( Fullname );
+		public Type RefType => Type.GetType( Fullname );
+
+		[HideInInspector]
+		public string Fullname;
+	}
+
+	public class TypeReferenceAttribute : Attribute
+	{
+		private readonly Type _typeConstraint;
+
+		public TypeReferenceAttribute( Type typeConstraint )
+		{
+			_typeConstraint = typeConstraint;
+		}
+
+		public IEnumerable<Type> GetTypes()
+		{
+			var query = _typeConstraint.Assembly.GetTypes()
+				.Where( x => !x.IsInterface )                           // Excludes BaseClass
+				.Where( x => !x.IsAbstract )                            // Excludes BaseClass
+				.Where( x => !x.IsGenericTypeDefinition )               // Excludes C1<>
+				.Where( x => _typeConstraint.IsAssignableFrom( x ) );   // Excludes classes not inheriting from BaseClas
+
+			return query;
+		}
+	}
+}

9fc189b997b462bc80e57b083f4feb45b689f821
Max <himmelmax@gmail.com>
2022-11-22 08:14:53 -0800

    added listeners for specific vfx signals

---
+++ Assets/Scripts/Installers/VFX/AttackedVfxListenerSettings.cs
@@ -0,0 +1,10 @@
+﻿using Minipede.Gameplay.Weapons;
+using UnityEngine;
+
+namespace Minipede.Installers
+{
+	[CreateAssetMenu( menuName = "VFX/Attacked Listener" )]
+	public class AttackedVfxListenerSettings : VfxListenerSettings<AttackedSignal>
+	{
+	}
+}
---
+++ Assets/Scripts/Installers/VFX/DamagedVfxListenerSettings.cs
@@ -0,0 +1,10 @@
+﻿using Minipede.Gameplay;
+using UnityEngine;
+
+namespace Minipede.Installers
+{
+	[CreateAssetMenu( menuName = "VFX/Damaged Listener" )]
+	public class DamagedVfxListenerSettings : VfxListenerSettings<DamagedSignal>
+	{
+	}
+}

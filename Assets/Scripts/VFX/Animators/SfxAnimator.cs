65aadd20382f1a749785b1a18743323d68c5ba51
Max <himmelmax@gmail.com>
2022-11-23 22:26:00 -0800

    created new VFX for SFX
    (gotta rename this VFX framework to something more generic)

---
+++ Assets/Scripts/VFX/Animators/SfxAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Minipede.Gameplay.Audio;
+
+namespace Minipede.Gameplay.Vfx
+{
+	public class SfxAnimator : IVfxAnimator
+	{
+		private readonly Settings _settings;
+		private readonly IAudioController _audioController;
+
+		public SfxAnimator( Settings settings,
+			IAudioController audioController )
+		{
+			_settings = settings;
+			_audioController = audioController;
+		}
+
+		public void Play( IVfxSignal signal )
+		{
+			_audioController.PlayOneShot( _settings.EventReference.EventName, signal.Position );
+		}
+
+		[System.Serializable]
+		public struct Settings : IVfxAnimator.Settings
+		{
+			public Type AnimatorType => typeof( SfxAnimator );
+
+			public AudioEventReference EventReference;
+		}
+	}
+}

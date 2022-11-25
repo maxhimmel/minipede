d34ce6b13fa67f0aa5dbb014a1946f8121cd3919
Max <himmelmax@gmail.com>
2022-11-24 16:06:14 -0800

    juiced up audio bank editor

--- Assets/Scripts/Audio/AudioBank.cs
+++ Assets/Scripts/Audio/AudioBank.cs
@@ -2,8 +2,11 @@ using Sirenix.OdinInspector;
 using UnityEngine;
 using Sirenix.Utilities;
 using System.Collections.Generic;
+using UnityEngine.Audio;
+using System.Linq;
 
 #if UNITY_EDITOR
+using UnityEditor;
 using Sirenix.Utilities.Editor;
 #endif
 
@@ -14,9 +17,13 @@ namespace Minipede.Gameplay.Audio
     {
         public string Category => _category;
         public IEnumerable<BankEvent> Events => _events;
+        private bool IsCategoryValid => !string.IsNullOrEmpty( Category );
 
+        [ValueDropdown( "GetMixerGroupNames" )]
+        [InfoBox( "Category must match a mixer group name.", VisibleIf = "@!IsCategoryValid", InfoMessageType = InfoMessageType.Error )]
         [SerializeField] private string _category;
 
+        [ShowIf( "IsCategoryValid" )]
         [ListDrawerSettings( OnTitleBarGUI = "DrawSortButton" )]
         [OnValueChanged( "OnBankEntryChanged", IncludeChildren = true )]
         [SerializeField] private BankEvent[] _events;
@@ -27,6 +34,20 @@ namespace Minipede.Gameplay.Audio
 		}
 
 #if UNITY_EDITOR
+        private IEnumerable<string> GetMixerGroupNames()
+		{
+            string[] guids = AssetDatabase.FindAssets( "MasterMixer" );
+            if ( guids == null || guids.Length <= 0 )
+			{
+                throw new System.NotImplementedException( "Create an AudioMixer named 'MasterMixer'." );
+			}
+
+            string path = AssetDatabase.GUIDToAssetPath( guids[0] );
+            AudioMixer mainMixer = AssetDatabase.LoadAssetAtPath<AudioMixer>( path );
+
+            return mainMixer.FindMatchingGroups( "Master" ).Select( group => group.name );
+		}
+
         private void OnBankEntryChanged()
 		{
             foreach ( var entry in _events )

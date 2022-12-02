040de5b323464b75d0ba15197a1e3e67655aca3c
Max <himmelmax@gmail.com>
2022-12-02 15:17:28 -0800

    created instance based loot box installers
    to be attached per actor that spawns loot (enemy, block, etc)

---
+++ Assets/Scripts/Installers/Pawns/LootBoxInstaller.cs
@@ -0,0 +1,21 @@
+﻿using Minipede.Gameplay.Treasures;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Installers
+{
+	[CreateAssetMenu( menuName = "Loot Box" )]
+	public class LootBoxInstaller : ScriptableObjectInstaller
+	{
+		[HideLabel, BoxGroup]
+		[SerializeField] private LootBox.Settings _settings;
+
+		public override void InstallBindings()
+		{
+			Container.Bind<LootBox>()
+				.AsSingle()
+				.WithArguments( _settings );
+		}
+	}
+}

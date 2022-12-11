f30b0978b115f917e64f5f9168eb84a687e6409e
Max Himmel <himmelmax@gmail.com>
2022-12-11 09:21:47 -0800

    invincible damageable can also be force killed
    does this make sense????

--- Assets/Scripts/Actors/Invincible.cs
+++ Assets/Scripts/Actors/Invincible.cs
@@ -1,20 +1,28 @@
-using System;
+using Minipede.Gameplay.Fx;
+using Minipede.Utility;
 using UnityEngine;
+using Zenject;
 
 namespace Minipede.Gameplay
 {
 	public class Invincible : IDamageController
 	{
-		event IDamageController.OnHit IDamageController.Damaged { add { } remove { } }			  
-		event IDamageController.OnHit IDamageController.Died { add { } remove { } }
+		event IDamageController.OnHit IDamageController.Damaged { add { } remove { } }
+		public event IDamageController.OnHit Died;
 
 		private readonly Rigidbody2D _body;
+		private readonly HealthController _health;
+		private readonly SignalBus _signalBus;
 		private readonly bool _logDamage;
 
-		public Invincible( Rigidbody2D body, 
+		public Invincible( Rigidbody2D body,
+			HealthController health,
+			SignalBus signalBus,
 			bool logDamage )
 		{
 			_body = body;
+			_health = health;
+			_signalBus = signalBus;
 			_logDamage = logDamage;
 		}
 
@@ -27,6 +35,23 @@ namespace Minipede.Gameplay
 			}
 
 			return 0;
+		}
+
+		public void ForceKill( Transform instigator, Transform causer, DamageDatum data )
+		{
+			_health.ForceKill( data );
+
+			if ( _logDamage )
+			{
+				Debug.Log( $"'<b>{_body.name}</b>' has been force-killed from '<b>{instigator?.name}</b>' using '<b>{causer?.name}</b>'." );
+			}
+
+			Died?.Invoke( _body, _health );
+
+			_signalBus.TryFireId( "Died", new FxSignal(
+				position: _body.position,
+				direction: (_body.position - causer.position.ToVector2()).normalized
+			) );
 		}
 	}
 }

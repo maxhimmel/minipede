7aa8bc817906ac8557097bc2743f0749c5ddc374
Max Himmel <himmelmax@gmail.com>
2022-12-14 15:45:07 -0800

    created status effect controller
    with poison effect and installer potential for damageables to be able to take status effect dmg

---
+++ Assets/Scripts/Actors/PoisonInvoker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Minipede.Gameplay
+{
+    public class PoisonInvoker : StatusEffectInvoker
+    {
+		public PoisonInvoker( Settings settings,
+            StatusEffectController statusController ) 
+            : base( settings, statusController )
+        {
+		}
+
+        protected override DamageResult InternalInvoke( IDamageable damageable, Transform instigator, Transform causer )
+        {
+            return new DamageResult()
+			{
+                DamageTaken = damageable.Health.Reduce( GetSettings<Settings>().Damage ),
+                FxEventName = "Poisoned"
+			};
+		}
+
+        [System.Serializable]
+        public new struct Settings : ISettings
+        {
+            public Type DamageType => typeof( PoisonInvoker );
+
+            float ISettings.ApplyRate => ApplyRate;
+            bool ISettings.CanExpire => CanExpire;
+            float ISettings.Duration => Duration;
+
+			public int Damage;
+            public float ApplyRate;
+
+            [HorizontalGroup( GroupID = "Expiration" )]
+            public bool CanExpire;
+            [HorizontalGroup( GroupID = "Expiration" ), EnableIf( "CanExpire" )]
+            public float Duration;
+        }
+    }
+}
---
+++ Assets/Scripts/Installers/Pawns/StatusEffectReceiverInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Minipede.Gameplay;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Installers
+{
+	[CreateAssetMenu]
+	public class StatusEffectReceiverInstaller : ScriptableObjectInstaller
+	{
+		public override void InstallBindings()
+		{
+			Container.BindInterfacesAndSelfTo<StatusEffectController>()
+				.AsSingle();
+		}
+	}
+}

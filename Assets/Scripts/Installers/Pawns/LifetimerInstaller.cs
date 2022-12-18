ebefa6a3400d3d2eb6a2c70a8f71533e200e0c8c
Max Himmel <himmelmax@gmail.com>
2022-12-17 16:28:24 -0800

    added monobehaviour style of lifetimer
    with factory installer

---
+++ Assets/Scripts/Installers/Pawns/LifetimerInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Minipede.Utility;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Installers
+{
+	[CreateAssetMenu]
+	public class LifetimerInstaller : ScriptableObjectInstaller
+	{
+		public override void InstallBindings()
+		{
+			Container.Bind<Lifetimer.Factory>()
+				.AsSingle();
+		}
+	}
+}

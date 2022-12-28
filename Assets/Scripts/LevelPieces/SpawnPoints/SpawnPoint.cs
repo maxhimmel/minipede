65c576b1a43fd717194d02f2bb2c1d43e90e70a4
Max Himmel <himmelmax@gmail.com>
2022-12-28 14:26:06 -0800

    added setters to getters

--- Assets/Scripts/LevelPieces/SpawnPoints/SpawnPoint.cs
+++ Assets/Scripts/LevelPieces/SpawnPoints/SpawnPoint.cs
@@ -5,9 +5,34 @@ namespace Minipede.Gameplay.LevelPieces
 {
 	public class SpawnPoint : MonoBehaviour,
 		IOrientation
-	{
-		public Vector2 Position => Parent.position;
-		public Quaternion Rotation => Parent.rotation;
-		public Transform Parent => transform;
-	}
+    {
+        public Vector2 Position {
+            get {
+                return transform.position;
+            }
+            set {
+                transform.position = value;
+            }
+        }
+
+        public Quaternion Rotation {
+            get {
+                return transform.rotation;
+            }
+            set {
+                transform.rotation = value;
+            }
+        }
+
+        public Transform Parent {
+            get { 
+                return _overrideTransform ?? transform; 
+            }
+            set {
+                _overrideTransform = value;
+            }
+        }
+
+        private Transform _overrideTransform;
+    }
 }

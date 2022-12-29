8c826e62e9f3550929b96ee25eadb32b080234f1
Max Himmel <himmelmax@gmail.com>
2022-12-29 15:55:19 -0800

    beacon injecting proper cleansed area helper

--- Assets/Scripts/Treasure/Beacon.cs
+++ Assets/Scripts/Treasure/Beacon.cs
@@ -10,18 +10,18 @@ namespace Minipede.Gameplay.Treasures
 {
 	public class Beacon : Collectable<Beacon>
 	{
-		public CleansedAreaSet CleansedAreaSet { get; private set; }
+		public ICleansedAreaProvider CleansedAreaProvider { get; private set; }
 
 		private PositionConstraint _constraint;
 		private Light2D _light;
 		private List<Collider2D> _colliders = new List<Collider2D>( 1 );
 
 		[Inject]
-		public void Construct( CleansedAreaSet cleansedAreaSet,
+		public void Construct( ICleansedAreaProvider cleansedAreaProvider,
 			PositionConstraint constraint,
 			Light2D light )
 		{
-			CleansedAreaSet = cleansedAreaSet;
+			CleansedAreaProvider = cleansedAreaProvider;
 			_constraint = constraint;
 			_light = light;
 		}

65670746eced1e1a1e68f71896975c8c8f32a3c1
Max Himmel <himmelmax@gmail.com>
2023-01-08 14:45:00 -0800

    easier spawning for testing

--- Assets/Scripts/Testing/DummySpawner.cs
+++ Assets/Scripts/Testing/DummySpawner.cs
@@ -1,4 +1,3 @@
-using Minipede.Utility;
 using UnityEngine;
 using Zenject;
 
@@ -6,6 +5,8 @@ namespace Minipede.Gameplay.Enemies.Spawning
 {
     public class DummySpawner : MonoBehaviour
 	{
+		[SerializeField] private Enemy _enemy;
+
 		private EnemySpawnBuilder _spawnBuilder;
 
 		[Inject]
@@ -18,11 +19,94 @@ namespace Minipede.Gameplay.Enemies.Spawning
 		{
 			if ( Input.GetKeyDown( KeyCode.Return ) )
 			{
-				_spawnBuilder.Build<SpiderController>()
-					.WithPlacement( transform.ToData() )
-					.WithSpawnBehavior()
-					.Create();
+				switch ( _enemy )
+				{
+					case Enemy.Bee:
+						_spawnBuilder.Build<BeeController>()
+							//.WithPlacement( transform.ToData() )
+							.WithRandomPlacement()
+							.WithSpawnBehavior()
+							.Create();
+						return;
+
+					case Enemy.Beetle:
+						_spawnBuilder.Build<BeetleController>()
+							//.WithPlacement( transform.ToData() )
+							.WithRandomPlacement()
+							.WithSpawnBehavior()
+							.Create();
+						return;
+
+					case Enemy.Dragonfly:
+						_spawnBuilder.Build<DragonflyController>()
+							//.WithPlacement( transform.ToData() )
+							.WithRandomPlacement()
+							.WithSpawnBehavior()
+							.Create();
+						return;
+
+					case Enemy.Earwig:
+						_spawnBuilder.Build<EarwigController>()
+							//.WithPlacement( transform.ToData() )
+							.WithRandomPlacement()
+							.WithSpawnBehavior()
+							.Create();
+						return;
+
+					case Enemy.Inchworm:
+						_spawnBuilder.Build<InchwormController>()
+							//.WithPlacement( transform.ToData() )
+							.WithRandomPlacement()
+							.WithSpawnBehavior()
+							.Create();
+						return;
+
+					case Enemy.Minipede:
+						_spawnBuilder.Build<MinipedeController>()
+							//.WithPlacement( transform.ToData() )
+							.WithRandomPlacement()
+							.WithSpawnBehavior()
+							.Create();
+						return;
+
+					case Enemy.Mosquito:
+						_spawnBuilder.Build<MosquitoController>()
+							//.WithPlacement( transform.ToData() )
+							.WithRandomPlacement()
+							.WithSpawnBehavior()
+							.Create();
+						return;
+
+					case Enemy.Segment:
+						_spawnBuilder.Build<SegmentController>()
+							//.WithPlacement( transform.ToData() )
+							.WithRandomPlacement()
+							.WithSpawnBehavior()
+							.Create();
+						return;
+
+					case Enemy.Spider:
+						_spawnBuilder.Build<SpiderController>()
+							//.WithPlacement( transform.ToData() )
+							.WithRandomPlacement()
+							.WithSpawnBehavior()
+							.Create();
+						return;
+				}
 			}
+		}
+
+		private enum Enemy
+		{
+			Bee,
+			Beetle,
+			Dragonfly,
+			Earwig,
+			Inchworm,
+			Minipede,
+			Mosquito,
+			Segment,
+			Spider
 		}
 	}
 }

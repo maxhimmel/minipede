64e31fe2bc13e1b43169b83c7236ff1c37a0ecb7
Max Himmel <himmelmax@gmail.com>
2023-01-09 14:19:58 -0800

    ability to assign damage to AOE from gameobject context installations

---
+++ Assets/Scripts/Installers/LevelPieces/DamageAOESettings.cs
@@ -0,0 +1,21 @@
+﻿using Minipede.Gameplay.Weapons;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Assets.Scripts.Installers.LevelPieces
+{
+	[CreateAssetMenu( menuName = AppHelper.MenuNamePrefix + "Weapons/DamageAOE" )]
+	public class DamageAOESettings : ScriptableObjectInstaller
+	{
+		[HideLabel, BoxGroup]
+		[SerializeField] private DamageAOE.Settings _settings;
+
+		public override void InstallBindings()
+		{
+			Container.BindInstance( _settings )
+				.AsSingle()
+				.WhenInjectedInto<DamageAOE>();
+		}
+	}
+}

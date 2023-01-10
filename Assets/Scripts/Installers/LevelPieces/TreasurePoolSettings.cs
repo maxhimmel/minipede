e26908bb8f1f803bf9a418367db5faea3e3d2724
Max Himmel <himmelmax@gmail.com>
2023-01-10 12:19:39 -0800

    treasure pooling!!!!!!!!

---
+++ Assets/Scripts/Installers/LevelPieces/TreasurePoolSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Minipede.Gameplay.Treasures;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Installers
+{
+	[CreateAssetMenu( menuName = AppHelper.MenuNamePrefix + "Collectables/TreasurePoolSettings" )]
+	public class TreasurePoolSettings : ScriptableObjectInstaller
+	{
+		[SerializeField] private List<TreasureFactoryBus.PoolSettings> _treasures;
+
+		public override void InstallBindings()
+		{
+			Container.Bind<TreasureFactoryBus>()
+				.AsSingle()
+				.WithArguments( _treasures );
+		}
+	}
+}

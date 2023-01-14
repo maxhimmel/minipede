0b76788b242839cc36866c3ea8a39d39c41704e3
Max Himmel <himmelmax@gmail.com>
2023-01-14 08:22:23 -0800

    easy enabler of mask interaction modes for sprites and particles

---
+++ Assets/Scripts/Utility/Masking/SpriteMaskInteractionEnabler.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Minipede.Utility
+{
+    [RequireComponent( typeof( SpriteRenderer ) )]
+    public class SpriteMaskInteractionEnabler : MaskInteractionEnabler<SpriteRenderer>
+    {
+		protected override void SetInteraction( SpriteRenderer maskedComponent, SpriteMaskInteraction interaction )
+		{
+			maskedComponent.maskInteraction = interaction;
+		}
+	}
+}

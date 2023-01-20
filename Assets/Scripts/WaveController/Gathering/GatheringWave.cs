df27432fa9f6d950dc148713ff77a3fd02a26f4e
Max Himmel <himmelmax@gmail.com>
2023-01-20 15:29:42 -0800

    reworked gathering wave controller to work w/new generic wave controller

---
+++ Assets/Scripts/WaveController/Gathering/GatheringWave.cs
@@ -0,0 +1,50 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Minipede.Gameplay.Waves
+{
+	public class GatheringWave : IWave
+	{
+		public string Id => "Gathering";
+		public bool IsRunning { get; private set; }
+
+		private readonly Settings _settings;
+
+		public GatheringWave( Settings settings )
+		{
+			_settings = settings;
+		}
+
+		public void Interrupt()
+		{
+			IsRunning = false;
+		}
+
+		public async UniTask<IWave.Result> Play()
+		{
+			IsRunning = true;
+
+			float timer = 0;
+			while ( timer < _settings.Duration )
+			{
+				timer += Time.deltaTime;
+				await UniTask.Yield( PlayerLoopTiming.Update, AppHelper.AppQuittingToken );
+
+				if ( !IsRunning )
+				{
+					return IWave.Result.Interrupted;
+				}
+			}
+
+			IsRunning = false;
+
+			return IWave.Result.Success;
+		}
+
+		[System.Serializable]
+		public struct Settings
+		{
+			public float Duration;
+		}
+	}
+}
---
+++ Assets/Scripts/WaveController/Gathering/GatheringWaveController.cs
@@ -0,0 +1,61 @@
+using System;
+using Minipede.Utility;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Gameplay.Waves
+{
+	public class GatheringWaveController : IInitializable,
+		IDisposable
+    {
+		private readonly Settings _settings;
+		private readonly WaveController _waveController;
+		private readonly GatheringWave _gatheringWave;
+
+		public GatheringWaveController( Settings settings,
+			WaveController waveController )
+		{
+			_settings = settings;
+			_waveController = waveController;
+
+			_gatheringWave = new GatheringWave( settings.Wave );
+		}
+
+		public void Dispose()
+		{
+			_waveController.Completed -= OnWaveCompleted;
+		}
+
+		public void Initialize()
+		{
+			_waveController.Completed += OnWaveCompleted;
+
+			InsertGatheringWave();
+		}
+
+		private void OnWaveCompleted( IWave wave, IWave.Result result )
+		{
+			if ( wave is GatheringWave )
+			{
+				InsertGatheringWave();
+			}
+		}
+
+		private void InsertGatheringWave()
+		{
+			int gatheringDelay = _settings.WaveDelay.Random( false );
+			_waveController.Insert( _waveController.CurrentWaveIndex + gatheringDelay, _gatheringWave );
+		}
+
+		[System.Serializable]
+		public struct Settings
+		{
+			[MinMaxSlider( 1, 10 )]
+			public Vector2Int WaveDelay;
+
+			[HideLabel]
+			public GatheringWave.Settings Wave;
+		}
+	}
+}

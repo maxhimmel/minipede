473d148b36b5e7d14ff124a96369e8975ba966da
Max Himmel <himmelmax@gmail.com>
2023-01-28 15:29:34 -0800

    added radial clamping to minimap

--- Assets/Scripts/UI/HUD/Minimap/Minimap.cs
+++ Assets/Scripts/UI/HUD/Minimap/Minimap.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Minipede.Gameplay.Player;
 using Minipede.Utility;
 using Sirenix.Utilities;
 using UnityEngine;
@@ -11,18 +10,17 @@ namespace Minipede.Gameplay.UI
     {
         [SerializeField] private RectTransform _container;
 
+		[SerializeField] private bool _isRadial;
+
 		private Camera _camera;
-		private PlayerController _playerController;
 		private MinimapMarkerFactoryBus _markerFactory;
 		private Dictionary<Transform, MinimapMarker> _markers;
 
 		[Inject]
 		public void Construct( Camera camera,
-			PlayerController playerController,
 			MinimapMarkerFactoryBus markerFactory )
 		{
 			_camera = camera;
-			_playerController = playerController;
 			_markerFactory = markerFactory;
 
 			_markers = new Dictionary<Transform, MinimapMarker>();
@@ -32,7 +30,7 @@ namespace Minipede.Gameplay.UI
 		{
 			var newMarker = _markerFactory.Create( 
 				markerPrefab, 
-				new Orientation(avatar.position, GetMarkerDirection( avatar ), _container ) 
+				new Orientation(avatar.position, Quaternion.identity, _container ) 
 			);
 
 			_markers.Add( avatar, newMarker );
@@ -49,6 +47,8 @@ namespace Minipede.Gameplay.UI
 		private void LateUpdate()
 		{
 			var containerRect = _container.rect;
+			var radius = 0.5f * Mathf.Min( containerRect.width, containerRect.height );
+			var center = new Vector2( containerRect.width * 0.5f, containerRect.height * 0.5f );
 
 			foreach ( var avatar in _markers.Keys )
 			{
@@ -56,21 +56,32 @@ namespace Minipede.Gameplay.UI
 				var viewportPos = _camera.WorldToViewportPoint( avatarPos )
 					.Clamp( Vector2.zero, Vector2.one );
 
-				var marker = _markers[avatar];
-				marker.transform.localPosition = new Vector2()
+				var markerPos = new Vector2()
 				{
 					x = viewportPos.x * containerRect.width,
 					y = viewportPos.y * containerRect.height
 				};
-				marker.transform.localRotation = GetMarkerDirection( avatar );
+
+				if ( _isRadial )
+				{
+					var markerDir = markerPos - center;
+					if ( markerDir.sqrMagnitude > radius * radius )
+					{
+						markerPos = center + markerDir.normalized * radius;
+					}
+				}
+
+				var marker = _markers[avatar];
+				marker.transform.localPosition = markerPos;
+				marker.transform.localRotation = (markerPos - center).ToLookRotation();//GetMarkerDirection( avatar );
 			}
 		}
 
-		private Quaternion GetMarkerDirection( Transform avatar )
-		{
-			return _container.InverseTransformDirection(
-				avatar.position.ToVector2() - _playerController.Position
-			).ToLookRotation();
-		}
+		//private Quaternion GetMarkerDirection( Transform avatar )
+		//{
+		//	return _container.InverseTransformDirection(
+		//		avatar.position.ToVector2() - _playerController.Position
+		//	).ToLookRotation();
+		//}
 	}
 }

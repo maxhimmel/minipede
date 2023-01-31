5e644767bbbfa0d13fc0e26fd70bce83ee6989f8
Max Himmel <himmelmax@gmail.com>
2023-01-31 11:01:12 -0800

    created menu and sub menu prefabs

---
+++ Assets/Scripts/UI/Settings/AudioMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Minipede.Gameplay.UI
+{
+	public class AudioMenu : Menu
+	{
+		[SerializeField] private Button _backButton;
+
+		[Header( "SFX" )]
+		[SerializeField] private Button _reduceSfxButton;
+		[SerializeField] private Button _increaseSfxButton;
+
+		[Header( "Music" )]
+		[SerializeField] private Button _reduceMusicButton;
+		[SerializeField] private Button _increaseMusicButton;
+
+		public override void Initialize()
+		{
+			base.Initialize();
+
+			_backButton.onClick.AddListener( _menuController.Pop );
+		}
+	}
+}
---
+++ Assets/Scripts/UI/Settings/SettingsMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Minipede.Gameplay.UI
+{
+	public class SettingsMenu : Menu
+	{
+		[Header( "Buttons" )]
+		[SerializeField] private Button _audioButton;
+		[SerializeField] private Button _backButton;
+
+		public override void Initialize()
+		{
+			base.Initialize();
+
+			_audioButton.onClick.AddListener( _menuController.Open<AudioMenu> );
+			_backButton.onClick.AddListener( _menuController.Pop );
+		}
+	}
+}

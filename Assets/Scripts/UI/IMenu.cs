e54d1189f2b35769cd313470c4154b8b1c3c6167
Max Himmel <himmelmax@gmail.com>
2023-01-31 10:56:59 -0800

    created basic menu framework

---
+++ Assets/Scripts/UI/IMenu.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace Minipede.Gameplay.UI
+{
+	public interface IMenu : IInitializable
+	{
+		string Title { get; }
+
+		void Show();
+		void Hide();
+	}
+}
---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Minipede.Gameplay.UI
+{
+	public class Menu : MonoBehaviour,
+		IMenu
+	{
+		public string Title => _title;
+
+		[SerializeField] private string _title = "Menu";
+
+		protected SignalBus _signalBus;
+		protected MenuController _menuController;
+
+		[Inject]
+		public void Construct( SignalBus signalBus,
+			MenuController menuController )
+		{
+			_signalBus = signalBus;
+			_menuController = menuController;
+		}
+
+		public virtual void Initialize()
+		{
+
+		}
+
+		public virtual void Show()
+		{
+			gameObject.SetActive( true );
+		}
+
+		public virtual void Hide()
+		{
+			gameObject.SetActive( false );
+		}
+	}
+}

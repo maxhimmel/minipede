bd5666bd4331d6016a5a3ef9a7925388f21634e1
Max Himmel <himmelmax@gmail.com>
2023-01-31 16:09:53 -0800

    audio, settings, and pause menu can be navigated with keyboard/joystick

---
+++ Assets/Scripts/UI/SetSelected.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Minipede.Gameplay.UI
+{
+	public class SetSelected : MonoBehaviour
+	{
+		private void OnEnable()
+		{
+			EventSystem.current.SetSelectedGameObject( gameObject );
+		}
+	}
+}

d4c78647cf81a04c6de9b5da2de81e60727a8385
Max Himmel <himmelmax@gmail.com>
2023-02-21 20:37:29 -0800

    guns can knock things backwards!

---
+++ Assets/Scripts/Weapons/Gun/KnockbackProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Minipede.Gameplay.Weapons
+{
+	public class KnockbackProcessor : IFireEndProcessor
+	{
+		private readonly Settings _settings;
+		private readonly IPushable _pushable;
+		private readonly Transform _owner;
+
+		public KnockbackProcessor( Settings settings,
+			IPushable pushable,
+			Transform owner )
+		{
+			_settings = settings;
+			_pushable = pushable;
+			_owner = owner;
+		}
+
+		public void FireEnding()
+		{
+			_pushable.Push( -_owner.up * _settings.Impulse );
+		}
+
+		[System.Serializable]
+		public struct Settings : IGunModule
+		{
+			public Type ModuleType => typeof( KnockbackProcessor );
+
+			public float Impulse;
+		}
+	}
+}

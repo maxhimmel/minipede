1ee42973c34af89e1e6a154e069f7ddac62e4a72
Max Himmel <himmelmax@gmail.com>
2023-02-23 11:22:04 -0800

    moved max speed decorator installation directly into enemy installer

--- Assets/Scripts/Installers/Enemies/EnemyControllerInstaller.cs
+++ Assets/Scripts/Installers/Enemies/EnemyControllerInstaller.cs
@@ -1,4 +1,6 @@
 using Minipede.Gameplay.Enemies;
+using Minipede.Gameplay.Movement;
+using Minipede.Utility;
 using UnityEngine;
 using Zenject;
 
@@ -19,6 +21,15 @@ namespace Minipede.Installers
 			Container.Bind<Transform>()
 				.FromComponentOnRoot()
 				.AsSingle();
+
+			/* --- */
+
+			Container.Bind<Scalar>()
+				.FromResolveGetter<DiContainer>( container => container.ResolveId<Scalar>( "EnemySpeedScalar" ) );
+
+			// TODO: Is this necessary now that structs have been converted into classes?
+			Container.Decorate<IMaxSpeed>()
+				.With<MaxSpeedDecorator>();
 		}
 	}
 }

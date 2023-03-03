cac39e1904e34d4f80772165a31ad4cc7a0f6786
Max Himmel <himmelmax@gmail.com>
2023-03-03 10:40:17 -0800

    added active list of enemies and blocks

---
+++ Assets/Scripts/Enemies/ActiveEnemies.cs
@@ -0,0 +1,12 @@
+﻿using Minipede.Utility;
+using Zenject;
+
+namespace Minipede.Gameplay.Enemies
+{
+	public class ActiveEnemies : ActiveList<EnemyController, EnemySpawnedSignal, EnemyDestroyedSignal>
+	{
+		public ActiveEnemies( SignalBus signalBus ) : base( signalBus )
+		{
+		}
+	}
+}
---
+++ Assets/Scripts/LevelPieces/Blocks/ActiveBlocks.cs
@@ -0,0 +1,12 @@
+﻿using Minipede.Utility;
+using Zenject;
+
+namespace Minipede.Gameplay.LevelPieces
+{
+	public class ActiveBlocks : ActiveList<Block, BlockSpawnedSignal, BlockDestroyedSignal>
+	{
+		public ActiveBlocks( SignalBus signalBus ) : base( signalBus )
+		{
+		}
+	}
+}

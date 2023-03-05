bd4b87f9f54393ffb766b853e3ab71313956d4e5
Max Himmel <himmelmax@gmail.com>
2023-03-04 17:34:11 -0800

    minor cleanup to level balance installation

--- Assets/Scripts/Installers/LevelPieces/LevelBalanceInstaller.cs
+++ Assets/Scripts/Installers/LevelPieces/LevelBalanceInstaller.cs
@@ -8,21 +8,20 @@ namespace Minipede.Installers
 {
 	public class LevelBalanceInstaller : MonoInstaller
 	{
-		[BoxGroup( "Timing" ), HideLabel]
-		[SerializeField] private LevelCycleTimer.Settings _timing;
-
-		[BoxGroup( "Balancing" ), HideLabel]
+		[BoxGroup, HideLabel]
 		[SerializeField] private LevelBalanceController.Settings _balancing;
+		[BoxGroup, HideLabel]
+		[SerializeField] private LevelCycleTimer.Settings _timing;
 
 		public override void InstallBindings()
 		{
-			Container.BindInterfacesAndSelfTo<LevelCycleTimer>()
-				.AsSingle()
-				.WithArguments( _timing );
-
 			Container.BindInterfacesAndSelfTo<LevelBalanceController>()
 				.AsSingle()
 				.WithArguments( _balancing );
+
+			Container.BindInterfacesAndSelfTo<LevelCycleTimer>()
+				.AsSingle()
+				.WithArguments( _timing );
 
 			/* --- */
 

2c4c2c6ee6119d95e4cd4f2808f3bb4edfb0b095
Max Himmel <himmelmax@gmail.com>
2023-03-04 17:35:08 -0800

    added enemy balance asset
    + attach to enemy settings (per enemy)
    + balances their speed and health over level balance cycles

---
+++ Assets/Scripts/Enemies/EnemyBalances.cs
@@ -0,0 +1,22 @@
+﻿using Minipede.Utility;
+using UnityEngine;
+
+namespace Minipede.Gameplay.Enemies
+{
+	[CreateAssetMenu( menuName = AppHelper.MenuNamePrefix + "Enemies/Balance Table" )]
+	public class EnemyBalances : ScriptableObject
+	{
+		[SerializeField] private CurveEvaluator _speed = new CurveEvaluator( AnimationCurve.Linear( 0, 1, 10, 1 ) );
+		[SerializeField] private CurveEvaluator _health = new CurveEvaluator( AnimationCurve.Linear( 0, 0, 10, 0 ) );
+
+		public float GetSpeed( int cycle, float defaultValue )
+		{
+			return _speed.Evaluate( cycle, defaultValue );
+		}
+
+		public int GetHealth( int cycle, int defaultValue )
+		{
+			return Mathf.FloorToInt( _health.Evaluate( cycle, defaultValue ) );
+		}
+	}
+}

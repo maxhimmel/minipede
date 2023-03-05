3339527214caee9209b6bee1bd959a5e114e8ebc
Max Himmel <himmelmax@gmail.com>
2023-03-04 18:08:08 -0800

    created mushroom balancing lookup class and asset

---
+++ Assets/Scripts/LevelPieces/Blocks/MushroomBalances.cs
@@ -0,0 +1,16 @@
+﻿using Minipede.Utility;
+using UnityEngine;
+
+namespace Minipede.Assets.Scripts.LevelPieces.Blocks
+{
+	[CreateAssetMenu( menuName = AppHelper.MenuNamePrefix + "Misc/MushroomBalanceTable" )]
+	public class MushroomBalances : ScriptableObject
+	{
+		[SerializeField] private CurveEvaluator _health;
+
+		public int GetHealth( int cycle, int defaultValue )
+		{
+			return Mathf.FloorToInt( _health.Evaluate( cycle, defaultValue ) );
+		}
+	}
+}

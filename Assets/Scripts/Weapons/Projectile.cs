9e94853ea004300cd3dbd8b7c593b8d4e06b660d
Max Himmel <himmelmax@gmail.com>
2023-03-28 12:40:46 -0700

    cleaned up encapsulation

--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -44,7 +44,7 @@ namespace Minipede.Gameplay.Weapons
 			}
 		}
 
-		public void OnDamagedOther( DamageDeliveredSignal message )
+		private void OnDamagedOther( DamageDeliveredSignal message )
 		{
 			Dispose();
 		}

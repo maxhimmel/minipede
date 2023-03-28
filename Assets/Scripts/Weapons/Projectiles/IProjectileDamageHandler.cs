84f8c0c7560a233d7dcf43260368463457023cf8
Max Himmel <himmelmax@gmail.com>
2023-03-28 13:35:02 -0700

    created interface and concrete for projectile damage handler
    + projectiles can pierce things?!

---
+++ Assets/Scripts/Weapons/Projectiles/IProjectileDamageHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Minipede.Gameplay.Weapons
+{
+	public interface IProjectileDamageHandler : IDisposable
+	{
+		void Handle( Projectile projectile, DamageDeliveredSignal signal );
+
+		public interface ISettings
+		{
+			System.Type HandlerType { get; }
+		}
+	}
+}
---
+++ Assets/Scripts/Weapons/Projectiles/ProjectilePierceHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Sirenix.OdinInspector;
+
+namespace Minipede.Gameplay.Weapons
+{
+	public class ProjectilePierceHandler : IProjectileDamageHandler
+	{
+		private readonly Settings _settings;
+
+		private int _pierceCount;
+
+		public ProjectilePierceHandler( Settings settings )
+		{
+			_settings = settings;
+		}
+
+		public void Handle( Projectile projectile, DamageDeliveredSignal signal )
+		{
+			if ( _settings.IsUnlimited )
+			{
+				return;
+			}
+
+			if ( _pierceCount++ >= _settings.Depth )
+			{
+				projectile.Dispose();
+			}
+		}
+
+		public void Dispose()
+		{
+			_pierceCount = 0;
+		}
+
+		[System.Serializable]
+		public class Settings : IProjectileDamageHandler.ISettings
+		{
+			public Type HandlerType => typeof( ProjectilePierceHandler );
+
+			[HorizontalGroup]
+			public bool IsUnlimited;
+
+			[HorizontalGroup]
+			[MinValue( 0 ), DisableIf( "IsUnlimited" )]
+			public int Depth;
+		}
+	}
+}

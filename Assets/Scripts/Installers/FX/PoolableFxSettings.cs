17a9efdcc028fa44609ae1e9542959d13dcf89f9
Max Himmel <himmelmax@gmail.com>
2023-04-19 10:33:33 -0700

    created poolable VFX framework
    + with particle system implementation

---
+++ Assets/Scripts/Installers/FX/PoolableFxSettings.cs
@@ -0,0 +1,19 @@
+﻿using Minipede.Gameplay.Fx;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Installers
+{
+	public class PoolableFxSettings : MonoInstaller
+	{
+		[HideLabel]
+		[SerializeField] private PoolableFx.Settings _settings;
+
+		public override void InstallBindings()
+		{
+			Container.BindInstance( _settings )
+				.AsSingle();
+		}
+	}
+}

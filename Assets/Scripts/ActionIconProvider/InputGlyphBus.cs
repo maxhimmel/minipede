8a72c760e41c7adee6491b83c1732882d3fe50ed
Max Himmel <himmelmax@gmail.com>
2023-04-24 08:45:45 -0700

    WIP input glyph bus framework and installation

---
+++ Assets/Scripts/ActionIconProvider/InputGlyphBus.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Rewired;
+
+namespace Minipede
+{
+	public class InputGlyphBus
+	{
+		private readonly Settings _settings;
+		private readonly Dictionary<string, InputGlyphs> _glyphs;
+
+		public InputGlyphBus( Settings settings )
+		{
+			_settings = settings;
+			_glyphs = settings.Glyphs.ToDictionary( g => g.InputGuid );
+		}
+
+		public string GetGlyph( int actionId, AxisRange axisRange = AxisRange.Full )
+		{
+			string controllerGuid = "";
+			int elementId = actionId;
+
+			if ( !_glyphs.TryGetValue( controllerGuid, out var glyph ) )
+			{
+				glyph = _settings.Fallback;
+			}
+
+			return glyph.GetGlyph( elementId, axisRange );
+		}
+
+		[System.Serializable]
+		public class Settings
+		{
+			public InputGlyphs Fallback;
+			public InputGlyphs[] Glyphs;
+		}
+	}
+}
---
+++ Assets/Scripts/ActionIconProvider/Installers/InputGlyphInstaller.cs
@@ -0,0 +1,21 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Installers
+{
+	[CreateAssetMenu( menuName = "Tools/Input/Glyph Installer" )]
+	public class InputGlyphInstaller : ScriptableObjectInstaller
+	{
+		[HideLabel]
+		[SerializeField] private InputGlyphBus.Settings _settings;
+
+		public override void InstallBindings()
+		{
+			Container.Bind<InputGlyphBus>()
+				.AsSingle()
+				.WithArguments( _settings )
+				.NonLazy();
+		}
+	}
+}

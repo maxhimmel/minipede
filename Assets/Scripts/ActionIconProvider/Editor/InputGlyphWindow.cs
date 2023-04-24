d677e268d4b27b4698f65b64f86a55ed4f64c9f1
Max Himmel <himmelmax@gmail.com>
2023-04-24 08:43:53 -0700

    updated rewired glyph importer window

---
+++ Assets/Scripts/ActionIconProvider/Editor/InputGlyphWindow.cs
@@ -0,0 +1,36 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Minipede.Editor
+{
+	public class InputGlyphWindow : OdinEditorWindow
+	{
+		[HideLabel, TabGroup( "Controllers", Icon = SdfIconType.Controller )]
+		public ControllerGlyphImporter ControllerImporter = new ControllerGlyphImporter();
+
+		[HideLabel, TabGroup( "Keyboard", Icon = SdfIconType.Keyboard )]
+		public KeyboardGlyphImporter KeyboardImporter = new KeyboardGlyphImporter();
+
+		[HideLabel, TabGroup( "Mouse", Icon = SdfIconType.Mouse2 )]
+		public MouseGlyphImporter MouseImporter = new MouseGlyphImporter();
+
+		[MenuItem( "Tools/Input/Glyph Importer" )]
+		private static void OpenWindow()
+		{
+			var window = GetWindow<InputGlyphWindow>();
+			window.titleContent = new GUIContent( "Glyph Importer" );
+			window.Show();
+		}
+
+		protected override void Initialize()
+		{
+			base.Initialize();
+
+			ControllerImporter.Ingest();
+			KeyboardImporter.Ingest();
+			MouseImporter.Ingest();
+		}
+	}
+}

388f56977203682e5a76b3d2c2eb0f4bcad67347
Max Himmel <himmelmax@gmail.com>
2023-05-09 16:38:43 -0700

    added some fx animators for UI stuff
    + canvas group fading
    + anchor position tweening

--- Assets/Scripts/FX/Animators/IFxAnimator.cs
+++ Assets/Scripts/FX/Animators/IFxAnimator.cs
@@ -1,4 +1,6 @@
-﻿namespace Minipede.Gameplay.Fx
+﻿using System;
+
+namespace Minipede.Gameplay.Fx
 {
 	public interface IFxAnimator
 	{
@@ -7,6 +9,12 @@
 		public interface ISettings
 		{
 			System.Type AnimatorType { get; }
+		}
+
+		public class Settings<TAnimator> : ISettings
+			where TAnimator : IFxAnimator
+		{
+			public Type AnimatorType => typeof( TAnimator );
 		}
 	}
 }

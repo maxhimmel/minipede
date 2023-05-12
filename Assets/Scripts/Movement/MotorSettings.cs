b1cd520592d1cabc7632b082153d2553a27c2f4b
Max Himmel <himmelmax@gmail.com>
2023-05-12 10:04:19 -0700

    removed TODO because we do not need to do it

--- Assets/Scripts/Movement/MotorSettings.cs
+++ Assets/Scripts/Movement/MotorSettings.cs
@@ -1,6 +1,5 @@
 ﻿namespace Minipede.Gameplay.Movement
 {
-	// TODO: Do we still need IMaxSpeed since we've converted to classes from structs?
 	[System.Serializable]
 	public class MotorSettings : IMaxSpeed
 	{

efa41705655e71625b09905d794c72c1016c66c4
Max Himmel <himmelmax@gmail.com>
2023-05-12 15:19:05 -0700

    created controller glyph request

---
+++ Assets/Scripts/Utility/Input/ControllerGlyphRequest.cs
@@ -0,0 +1,20 @@
+﻿using ControllerGlyph;
+using Rewired;
+using Sirenix.OdinInspector;
+
+namespace Minipede.Utility
+{
+	[System.Serializable]
+	public class ControllerGlyphRequest : ControllerGlyphBus.Request
+	{
+		[BoxGroup]
+		[PropertyOrder( -1 )]
+		[ActionIdProperty( typeof( ReConsts.Action ) )]
+		public int ActionId;
+
+		public override int GetActionId()
+		{
+			return ActionId;
+		}
+	}
+}

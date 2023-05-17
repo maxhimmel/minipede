e45eb0f0fddcdc7623703ae0ce1572400b54ffba
Max Himmel <himmelmax@gmail.com>
2023-05-17 15:36:25 -0700

    start menu uses mock player pawn locator
    + the explorer and ship never spawn here but the glyphs rely on that position

---
+++ Assets/Scripts/StartGame/StaticPlayerPawnLocator.cs
@@ -0,0 +1,26 @@
+﻿using Minipede.Utility;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Gameplay.Player
+{
+	public class StaticPlayerPawnLocator : IPlayerPawnLocator
+	{
+		public IOrientation Orientation { get; }
+
+		public StaticPlayerPawnLocator( [InjectOptional] Settings settings )
+		{
+			Orientation = settings != null ?
+				new Orientation( settings.Position, Quaternion.Euler( settings.EulerRotation ), settings.Parent )
+				: new Orientation();
+		}
+
+		[System.Serializable]
+		public class Settings
+		{
+			public Vector2 Position;
+			public Vector3 EulerRotation;
+			public Transform Parent;
+		}
+	}
+}

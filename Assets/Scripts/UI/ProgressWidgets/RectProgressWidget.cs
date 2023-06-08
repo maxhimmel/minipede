215f71c909b56885bfb283d3a2ec66f3ba811d2a
Max Himmel <himmelmax@gmail.com>
2023-06-08 15:35:23 -0700

    yet another progress widget

---
+++ Assets/Scripts/UI/ProgressWidgets/RectProgressWidget.cs
@@ -0,0 +1,28 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Minipede.Gameplay.UI
+{
+	public class RectProgressWidget : MonoProgressWidget
+	{
+		public override float NormalizedProgress => _progress;
+
+		[SerializeField] private RectTransform _rect;
+
+		[Space]
+		[SerializeField] private RectTransform.Edge _startEdge;
+		[SerializeField] private float _startSize;
+		[SerializeField] private float _endSize;
+
+		private float _progress;
+
+		[Button]
+		public override void SetProgress( float normalizedProgress )
+		{
+			_progress = normalizedProgress;
+
+			float size = Mathf.Lerp( _startSize, _endSize, normalizedProgress );
+			_rect.SetInsetAndSizeFromParentEdge( _startEdge, 0, size );
+		}
+	}
+}

fa3c7be3ccb6428c63fcda71bd3d4965adae18dd
Max Himmel <himmelmax@gmail.com>
2023-06-12 15:43:51 -0700

    created equipped gun model
    + singleton for easier UI access

---
+++ Assets/Scripts/Player/EquippedGunModel.cs
@@ -0,0 +1,20 @@
+﻿using Minipede.Gameplay.Weapons;
+
+namespace Minipede.Gameplay.Player
+{
+	public class EquippedGunModel
+	{
+		public event System.Action<Gun> Changed;
+
+		public Gun Gun { get; private set; }
+
+		public void SetGun( Gun newGun )
+		{
+			if ( Gun != newGun )
+			{
+				Gun = newGun;
+				Changed?.Invoke( newGun );
+			}
+		}
+	}
+}

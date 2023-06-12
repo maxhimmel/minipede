31786b6c06fcb8bad96b8c25cf812eed222ba01f
Max Himmel <himmelmax@gmail.com>
2023-06-12 13:18:03 -0700

    added resource type color changing widget

---
+++ Assets/Scripts/UI/Utility/ResourceTypeColorWidget.cs
@@ -0,0 +1,29 @@
+﻿using Minipede.Gameplay.Treasures;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Gameplay.UI
+{
+	public class ResourceTypeColorWidget : MonoBehaviour
+	{
+		[SerializeField] private MonoColorWidget _colorWidget;
+
+		private ResourceType _resource;
+
+		[Inject]
+		public void Construct( [InjectOptional] ResourceType resource )
+		{
+			SetResource( resource );
+		}
+
+		public void SetResource( ResourceType resource )
+		{
+			_resource = resource;
+
+			if ( resource != null )
+			{
+				_colorWidget.SetColor( resource.Color );
+			}
+		}
+	}
+}

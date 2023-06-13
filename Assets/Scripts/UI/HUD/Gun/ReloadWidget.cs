7acad60ac8444440d86bbc856c8e85fc0d770307
Max Himmel <himmelmax@gmail.com>
2023-06-13 11:15:01 -0700

    created reload widget

---
+++ Assets/Scripts/UI/HUD/Gun/ReloadWidget.cs
@@ -0,0 +1,34 @@
+﻿using Minipede.Gameplay.Weapons;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Gameplay.UI
+{
+	public class ReloadWidget : MonoBehaviour
+	{
+		[SerializeField] private MonoProgressWidget _progress;
+
+		private SignalBus _signalBus;
+
+		[Inject]
+		public void Construct( SignalBus signalBus )
+		{
+			_signalBus = signalBus;
+		}
+
+		private void OnDisable()
+		{
+			_signalBus.TryUnsubscribe<ReloadStateSignal>( OnReloadUpdated );
+		}
+
+		private void OnEnable()
+		{
+			_signalBus.Subscribe<ReloadStateSignal>( OnReloadUpdated );
+		}
+
+		private void OnReloadUpdated( ReloadStateSignal signal )
+		{
+			_progress.SetProgress( signal.NormalizedTimer );
+		}
+	}
+}

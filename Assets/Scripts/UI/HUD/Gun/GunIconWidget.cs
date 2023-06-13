bb5cc5dcda5e0b03dbaafa1a5bdbddf1f0d99de4
Max Himmel <himmelmax@gmail.com>
2023-06-13 11:15:10 -0700

    created gun icon widget

---
+++ Assets/Scripts/UI/HUD/Gun/GunIconWidget.cs
@@ -0,0 +1,37 @@
+﻿using Minipede.Gameplay.Player;
+using Minipede.Gameplay.Weapons;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Gameplay.UI
+{
+	public class GunIconWidget : MonoBehaviour
+	{
+		[SerializeField] private MonoSpriteWidget _icon;
+
+		private EquippedGunModel _gunModel;
+
+		[Inject]
+		public void Construct( EquippedGunModel gunModel )
+		{
+			_gunModel = gunModel;
+		}
+
+		private void OnDisable()
+		{
+			_gunModel.Changed -= ChangeIcon;
+		}
+
+		private void OnEnable()
+		{
+			_gunModel.Changed += ChangeIcon;
+
+			ChangeIcon( _gunModel.Gun );
+		}
+
+		private void ChangeIcon( Gun newGun )
+		{
+			_icon.SetSprite( newGun?.Icon );
+		}
+	}
+}

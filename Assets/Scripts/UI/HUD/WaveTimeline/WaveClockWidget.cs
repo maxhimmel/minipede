e6e8a162203830f86966ba723d62dcc9aed0a956
Max Himmel <himmelmax@gmail.com>
2023-06-15 12:12:10 -0700

    created a wave clock widget! (w/separate pieces)
    + color changing piece
    + icon changing piece

---
+++ Assets/Scripts/UI/HUD/WaveTimeline/WaveClockWidget.cs
@@ -0,0 +1,73 @@
+﻿using Minipede.Gameplay.LevelPieces;
+using Minipede.Gameplay.Waves;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Gameplay.UI
+{
+	public class WaveClockWidget : MonoBehaviour
+	{
+		[SerializeField] private MonoProgressWidget _clock;
+		[SerializeField] private MonoProgressWidget _dangerSection;
+
+		private LevelCycleTimer.ISettings _cycleSettings;
+		private NighttimeController.Settings _nighttimeSettings;
+		private SignalBus _signalBus;
+
+		[Inject]
+		public void Construct( LevelCycleTimer.ISettings cycleSettings,
+			NighttimeController.Settings nighttimeSettings,
+			SignalBus signalBus )
+		{
+			_cycleSettings = cycleSettings;
+			_nighttimeSettings = nighttimeSettings;
+			_signalBus = signalBus;
+
+			_dangerSection.SetProgress( GetTotalDangerDurationPercent() );
+		}
+
+		private void OnDisable()
+		{
+			_signalBus.TryUnsubscribe<LevelCycleProgressSignal>( OnDangerCycleUpdated );
+			_signalBus.TryUnsubscribe<NighttimeStateChangedSignal>( OnNighttimeUpdated );
+		}
+
+		private void OnEnable()
+		{
+			_signalBus.Subscribe<LevelCycleProgressSignal>( OnDangerCycleUpdated );
+			_signalBus.Subscribe<NighttimeStateChangedSignal>( OnNighttimeUpdated );
+		}
+
+		private void OnDangerCycleUpdated( LevelCycleProgressSignal signal )
+		{
+			int cycle = signal.Cycle % _nighttimeSettings.CycleRate;
+			float percentPerCycle = 1f / _nighttimeSettings.CycleRate;
+			float cycleOffset = cycle * percentPerCycle;
+
+			float dangerProgress = cycleOffset + Mathf.Lerp( 0, percentPerCycle, signal.NormalizedProgress );
+
+			_clock.SetProgress( GetTotalDangerDurationPercent() * dangerProgress );
+		}
+
+		private void OnNighttimeUpdated( NighttimeStateChangedSignal signal )
+		{
+			float clockProgress = Mathf.Lerp( GetTotalDangerDurationPercent(), 1, signal.NormalizedProgress );
+			_clock.SetProgress( clockProgress );
+		}
+
+		private float GetTotalDangerDurationPercent()
+		{
+			return GetTotalDangerDuration() / GetTotalDuration();
+		}
+
+		private float GetTotalDuration()
+		{
+			return _nighttimeSettings.Duration + GetTotalDangerDuration();
+		}
+
+		private float GetTotalDangerDuration()
+		{
+			return _cycleSettings.CycleDuration * _nighttimeSettings.CycleRate;
+		}
+	}
+}
---
+++ Assets/Scripts/UI/HUD/WaveTimeline/WaveColorWidget.cs
@@ -0,0 +1,73 @@
+﻿using Minipede.Gameplay.Waves;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Gameplay.UI
+{
+	public class WaveColorWidget : MonoBehaviour
+	{
+		[SerializeField] private MonoColorWidget _palette;
+
+		[HorizontalGroup]
+		[ToggleLeft]
+		[SerializeField] private bool _overrideCurrentMode;
+		
+		[HorizontalGroup]
+		[EnableIf( "_overrideCurrentMode" ), HideLabel]
+		[SerializeField] private Modes _overrideMode;
+
+		private NighttimeController _nighttime;
+		private WaveTimelineVisuals _visuals;
+		private SignalBus _signalBus;
+
+		[Inject]
+		public void Construct( NighttimeController nighttime,
+			WaveTimelineVisuals visuals,
+			SignalBus signalBus )
+		{
+			_nighttime = nighttime;
+			_visuals = visuals;
+			_signalBus = signalBus;
+		}
+
+		private void OnDisable()
+		{
+			if ( !_overrideCurrentMode )
+			{
+				_signalBus.TryUnsubscribe<NighttimeStateChangedSignal>( OnWaveModeChanged );
+			}
+		}
+
+		private void OnEnable()
+		{
+			if ( !_overrideCurrentMode )
+			{
+				_signalBus.Subscribe<NighttimeStateChangedSignal>( OnWaveModeChanged );
+			}
+
+			OnWaveModeChanged( new NighttimeStateChangedSignal()
+			{
+				IsNighttime = _overrideCurrentMode 
+					? _overrideMode == Modes.Nighttime
+					: _nighttime.IsNighttime
+			} );
+		}
+
+		private void OnWaveModeChanged( NighttimeStateChangedSignal signal )
+		{
+			var mode = signal.IsNighttime
+				? Modes.Nighttime
+				: Modes.Danger;
+
+			var visual = _visuals.GetVisual( mode.ToString() );
+			_palette.SetColor( visual.Color );
+		}
+
+		private enum Modes
+		{
+			Danger,
+			Nighttime
+		}
+	}
+}

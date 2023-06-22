6326eecd9e6a95bdac9672658bd2b6e4762d8436
Max Himmel <himmelmax@gmail.com>
2023-06-22 14:13:26 -0700

    game controller uses level start sequence!

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,6 +3,7 @@ using Cysharp.Threading.Tasks;
 using Minipede.Gameplay.Audio;
 using Minipede.Gameplay.LevelPieces;
 using Minipede.Gameplay.Player;
+using Minipede.Gameplay.StartSequence;
 using Minipede.Gameplay.Waves;
 using Minipede.Utility;
 using Zenject;
@@ -15,6 +16,7 @@ namespace Minipede.Gameplay
 	{
 		public bool IsReady { get; private set; }
 
+		private readonly LevelStartSequenceController _startSequence;
 		private readonly PlayerController _playerSpawnController;
 		private readonly LevelGenerator _levelGenerator;
 		private readonly IWaveController _waveController;
@@ -24,7 +26,8 @@ namespace Minipede.Gameplay
 		private readonly LevelCycleTimer _levelCycleTimer;
 		private readonly SceneLoader _sceneLoader;
 
-		public GameController( PlayerController playerSpawnController,
+		public GameController( LevelStartSequenceController startSequence,
+			PlayerController playerSpawnController,
 			LevelGenerator levelGenerator,
 			IWaveController waveController,
 			MushroomPopulationController mushroomPopulationController,
@@ -33,6 +36,7 @@ namespace Minipede.Gameplay
 			LevelCycleTimer levelCycleTimer,
 			SceneLoader sceneLoader )
 		{
+			_startSequence = startSequence;
 			_playerSpawnController = playerSpawnController;
 			_levelGenerator = levelGenerator;
 			_waveController = waveController;
@@ -58,7 +62,15 @@ namespace Minipede.Gameplay
 			await _audioBankLoader.LoadBanks();
 			await _levelGenerator.GenerateLevel().Cancellable( AppHelper.AppQuittingToken );
 
-			_playerSpawnController.RespawnPlayer();
+			_startSequence.CreateLighthouseMushrooms();
+			await _startSequence.Play( AppHelper.AppQuittingToken ).SuppressCancellationThrow();
+
+			if ( AppHelper.IsQuitting )
+			{
+				return;
+			}
+
+			//_playerSpawnController.RespawnPlayer();
 			_waveController.Play().Forget();
 			_levelCycleTimer.Play();
 

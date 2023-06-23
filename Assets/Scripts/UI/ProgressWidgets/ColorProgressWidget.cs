c16a464ab657d97a4065e154d43ed73a7f21012f
Max Himmel <himmelmax@gmail.com>
2023-06-23 11:06:55 -0700

    two new progress widgets
    + one blends colors
    + the other manages canvas groups alpha levels

---
+++ Assets/Scripts/UI/ProgressWidgets/ColorProgressWidget.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Minipede.Gameplay.UI
+{
+	public class ColorProgressWidget : MonoProgressWidget
+	{
+		public override float NormalizedProgress => _progress;
+
+		[SerializeField] private MonoColorWidget _colorWidget;
+
+		[Space]
+		[SerializeField] private Color _start = Color.black;
+		[SerializeField] private Color _end = Color.white;
+
+		private float _progress;
+
+		protected override void SetProgress_Internal( float normalizedProgress )
+		{
+			_progress = normalizedProgress;
+
+			_colorWidget.SetColor( Color.Lerp( _start, _end, normalizedProgress ) );
+		}
+	}
+}

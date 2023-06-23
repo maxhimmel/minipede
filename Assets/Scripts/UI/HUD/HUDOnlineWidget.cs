87e5cf0a17992b569b5faea0df3256781558d65a
Max Himmel <himmelmax@gmail.com>
2023-06-22 17:57:26 -0700

    updated HUD prefab with new HUD online flicker widget

---
+++ Assets/Scripts/UI/HUD/HUDOnlineWidget.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Minipede.Gameplay.StartSequence;
+using Minipede.Utility;
+using UnityEngine;
+using Zenject;
+
+namespace Minipede.Gameplay.UI
+{
+    public class HUDOnlineWidget : MonoBehaviour
+    {
+		[SerializeField] private CanvasGroup _fader;
+
+		[Space]
+		[SerializeField] private AnimationCurve _flickerCurve;
+
+        private SignalBus _signalBus;
+
+        [Inject]
+		public void Construct( SignalBus signalBus )
+		{
+            _signalBus = signalBus;
+		}
+
+		private void Start()
+		{
+			_fader.alpha = 0;
+		}
+
+		private void OnEnable()
+		{
+			_signalBus.TrySubscribe<HUDOnlineSignal>( OnHUDOnline );
+		}
+
+		private void OnDisable()
+		{
+			_signalBus.TryUnsubscribe<HUDOnlineSignal>( OnHUDOnline );
+		}
+
+		private void OnHUDOnline( HUDOnlineSignal signal )
+		{
+			FlickerOnline().Forget();
+		}
+
+		private async UniTaskVoid FlickerOnline()
+		{
+			float timer = 0;
+			float duration = _flickerCurve.GetDuration();
+
+			while ( timer < duration )
+			{
+				timer += Time.deltaTime;
+
+				_fader.alpha = _flickerCurve.Evaluate( timer );
+
+				await UniTask.Yield( PlayerLoopTiming.Update, AppHelper.AppQuittingToken );
+			}
+
+			_fader.alpha = 1;
+		}
+	}
+}

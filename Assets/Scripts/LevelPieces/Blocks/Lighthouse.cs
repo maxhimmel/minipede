a6ab0adb1d544fb7895905045130bfe09774cc3a
Max Himmel <himmelmax@gmail.com>
2023-06-28 16:18:48 -0700

    plant beacon controller now responsible for activating cleansed areas

--- Assets/Scripts/LevelPieces/Blocks/Lighthouse.cs
+++ Assets/Scripts/LevelPieces/Blocks/Lighthouse.cs
@@ -8,25 +8,11 @@ namespace Minipede.Gameplay.LevelPieces
 {
 	public class Lighthouse : Block
 	{
-		private CleansedArea.Factory _cleansedAreaFactory;
-
-		private CleansedArea _cleansedArea;
-
-		[Inject]
-		public void Construct( CleansedArea.Factory cleansedAreaFactory )
-		{
-			_cleansedAreaFactory = cleansedAreaFactory;
-		}
-
 		public void Equip( Beacon beacon )
 		{
 			beacon.StopFollowing();
 			beacon.Equip( _body );
 			beacon.Gun.Reload();
-
-			var cleansedAreaPrefab = beacon.CleansedAreaProvider.GetAsset();
-			_cleansedArea = _cleansedAreaFactory.Create( cleansedAreaPrefab, new Orientation( _body.position ) );
-			_cleansedArea.Activate();
 		}
 
 		public override void OnSpawned( IOrientation placement, IMemoryPool pool )

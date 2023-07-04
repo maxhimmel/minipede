a3919773046c67f210b45d063363d2f91e111b87
Max Himmel <himmelmax@gmail.com>
2023-07-03 17:57:20 -0700

    created enemy spawn warning assets and installed them
    + created marker prefab
    + added pool container under main pool prefab
    + created vfx for disposing marker

--- Assets/Scripts/Installers/UI/UIInstaller.cs
+++ Assets/Scripts/Installers/UI/UIInstaller.cs
@@ -1,19 +1,23 @@
 using System.Collections.Generic;
 using Minipede.Gameplay.UI;
-using Minipede.Utility;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using Zenject;
 
 namespace Minipede.Installers
 {
-    [CreateAssetMenu( menuName = AppHelper.MenuNamePrefix + "Managers/UIInstaller" )]
+	[CreateAssetMenu( menuName = AppHelper.MenuNamePrefix + "Managers/UIInstaller" )]
     public class UIInstaller : ScriptableObjectInstaller
     {
 		[SerializeField] private WaveTimelineVisuals.Settings _waveTimeline;
 
 		[FoldoutGroup( "Minimap" )]
 		[SerializeField] private List<MinimapMarkerFactoryBus.PoolSettings> _minimapMarkers;
+
+		[FoldoutGroup( "Spawn Warning" ), HideLabel]
+		[SerializeField] private EnemySpawnWarningWidget.Settings _enemySpawnWidget;
+		[FoldoutGroup( "Spawn Warning" )]
+		[SerializeField] private List<EnemySpawnMarkerFactoryBus.PoolSettings> _enemySpawnMarkers;
 
 		public override void InstallBindings()
 		{
@@ -24,6 +28,16 @@ namespace Minipede.Installers
 			Container.BindInterfacesAndSelfTo<MinimapMarkerFactoryBus>()
 				.AsSingle()
 				.WithArguments( _minimapMarkers );
+
+			/* --- */
+
+			Container.BindInterfacesAndSelfTo<EnemySpawnMarkerFactoryBus>()
+				.AsSingle()
+				.WithArguments( _enemySpawnMarkers );
+
+			Container.BindInterfacesTo<EnemySpawnWarningWidget>()
+				.AsSingle()
+				.WithArguments( _enemySpawnWidget );
 		}
 	}
 }

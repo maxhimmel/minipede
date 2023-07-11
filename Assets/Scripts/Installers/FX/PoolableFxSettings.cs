bb7967a0ce4b7976c552a2abb124c6a716d6e7c6
Max Himmel <himmelmax@gmail.com>
2023-07-11 10:27:42 -0700

    easier installation for VFX particle systems

--- Assets/Scripts/Installers/FX/PoolableFxSettings.cs
+++ Assets/Scripts/Installers/FX/PoolableFxSettings.cs
@@ -9,11 +9,31 @@ namespace Minipede.Installers
 	{
 		[HideLabel]
 		[SerializeField] private PoolableFx.Settings _settings;
+		[SerializeField] private Mode _mode;
 
 		public override void InstallBindings()
 		{
 			Container.BindInstance( _settings )
 				.AsSingle();
+
+			BindFxInstance();
+		}
+
+		private void BindFxInstance()
+		{
+			switch ( _mode )
+			{
+				case Mode.Particle:
+					Container.Bind<ParticleSystem>()
+						.FromMethod( GetComponentInChildren<ParticleSystem> )
+						.AsSingle();
+					break;
+			}
+		}
+
+		private enum Mode
+		{
+			Particle
 		}
 	}
 }

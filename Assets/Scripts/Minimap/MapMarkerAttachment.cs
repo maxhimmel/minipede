a65e2fdb6a3d5c417adedc020f1581ec2f98cabd
Max Himmel <himmelmax@gmail.com>
2023-07-11 11:42:33 -0700

    created map marker modules
    + these COULD BE attached under ship, shrapenl, etc prefabs

---
+++ Assets/Scripts/Minimap/MapMarkerAttachment.cs
@@ -0,0 +1,16 @@
+﻿using Minipede.Gameplay.UI;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Minipede.Gameplay.Minimap
+{
+	public class MapMarkerAttachment : MonoBehaviour,
+		IMapMarker
+	{
+		public Transform Avatar => _settings.Avatar;
+		public MinimapMarker MarkerPrefab => _settings.MarkerPrefab;
+
+		[HideLabel]
+		[SerializeField] private MapMarker _settings;
+	}
+}

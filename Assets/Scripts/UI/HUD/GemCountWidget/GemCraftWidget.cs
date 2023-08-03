b48509889a446ac89bd27ef14be3153a244af1d5
Max Himmel <himmelmax@gmail.com>
2023-08-03 15:26:01 -0700

    moved gem vacuum vfx into its own component

--- Assets/Scripts/UI/HUD/GemCountWidget/GemCraftWidget.cs
+++ Assets/Scripts/UI/HUD/GemCountWidget/GemCraftWidget.cs
@@ -1,5 +1,4 @@
 using Minipede.Gameplay.Treasures;
-using Minipede.Utility;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
@@ -9,7 +8,6 @@ namespace Minipede.Gameplay.UI
 	public class GemCraftWidget : MonoBehaviour
     {
 		[SerializeField] private Button _button;
-		[SerializeField] private ParticleSystem _collectVfx;
 
 		private ResourceType _resource;
 		private SignalBus _signalBus;
@@ -28,30 +26,6 @@ namespace Minipede.Gameplay.UI
 					ResourceType = _resource
 				} );
 			} );
-
-			var mainModule = _collectVfx.main;
-			mainModule.startColor = resource.Color;
-		}
-
-		private void OnEnable()
-		{
-			_signalBus.SubscribeId<ResourceAmountChangedSignal>( _resource, TryEmitCollectVfx );
-		}
-
-		private void OnDisable()
-		{
-			_signalBus.TryUnsubscribeId<ResourceAmountChangedSignal>( _resource, TryEmitCollectVfx );
-		}
-
-		// TODO: Separate this into bespoke widget
-		private void TryEmitCollectVfx( ResourceAmountChangedSignal signal )
-		{
-			if ( signal.PrevTotal < signal.TotalAmount )
-			{
-				var vfxShape = _collectVfx.shape;
-				vfxShape.position = signal.CollectionSource - _collectVfx.transform.position.ToVector2();
-				_collectVfx.Emit( 1 );
-			}
 		}
 	}
 }
